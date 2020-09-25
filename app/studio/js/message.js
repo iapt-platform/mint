b2457f2ad9d61d0a6cb781cb907b3022ecca7ae0
visuddhinanda <visuddhinanda@users.noreply.github.com>
2020-09-25 10:35:11 +0630

    :bug: 消息数据不使用旧的xml 节点，使用内存数组。

--- app/studio/js/message.js
+++ app/studio/js/message.js
@@ -186,7 +186,7 @@ function msg_read(msg_obj, status = null) {
       let oldStatus = msg_obj.read;
       msg_obj.read = status;
       for (let i = 0; i < gDocMsgList.length; i++) {
-        if (gDocMsgList[i].id == msg_obj.id) {
+        if (gDocMsgList[i].data.id == msg_obj.id) {
           gDocMsgList[i].read = status;
           break;
         }
