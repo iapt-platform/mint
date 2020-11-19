6bdfc8b5c184edcc62cebe05cc25488c00ddf4cf
visuddhinanda <visuddhinanda@users.noreply.github.com>
2020-11-19 13:59:12 +0630

    弹出式注释

---
+++ app/term/popup_note.js
@@ -0,0 +1,19 @@
+function popup_init() {
+	$("code").each(function () {
+		if ($(this).attr("init") != "1") {
+			if ($(this).text().length == 0) {
+				return;
+			}
+			const noteText = $(this).html();
+			$(this).html("");
+			if ($(this).offset().left < $(document.body).width() / 2) {
+				$(this).append('<div  class="popup_contence" style="left: -15px;">' + noteText + "</div>");
+				$(".popup_contence:after").css("left", "0");
+			} else {
+				$(this).append('<div  class="popup_contence" style="right: -15px;">' + noteText + "</div>");
+				$(".popup_contence:after").css("right", "0");
+			}
+			$(this).attr("init", "1");
+		}
+	});
+}
