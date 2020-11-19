a66f6d8ae3aed490f586916d54bd02511a519044
visuddhinanda <visuddhinanda@users.noreply.github.com>
2020-11-19 22:27:25 +0630

    路径放标题下面

--- app/widget/click_dropdown.js
+++ app/widget/click_dropdown.js
@@ -1,9 +1,15 @@
+var currDropdownMenu;
 function click_dropdown_init() {
-  $(".click_dropdown_button").click(function () {
-    $(this).siblings(".click_dropdown_content").first().show();
-  });
+	$(".click_dropdown_button").click(function () {
+		currDropdownMenu = this;
+		$(this).siblings(".click_dropdown_content").first().show();
+		$(document).one("click", function () {
+			$(currDropdownMenu).parent().children(".click_dropdown_content").hide();
+		});
+		event.stopPropagation();
+	});
 
-  $(".click_dropdown_cancel").click(function () {
-    $(this).parent().parent().hide();
-  });
+	$(".click_dropdown_content_inner").click(function () {
+		$(this).hide();
+	});
 }
