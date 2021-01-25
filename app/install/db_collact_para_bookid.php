7443f0df7eacd7d14148dfbd82bd5e8b91c6f402
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-01-25 20:32:00 +0630

    提取cs6段落信息

--- app/install/db_collact_para_bookid.php
+++ app/install/db_collact_para_bookid.php
@@ -1,30 +1,21 @@
 ﻿<?php
-require_once "install_head.php";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<h2>提取para?_??的内容</h2>
-<p><a href="index.php">Home</a></p>
-<?php
-
-$thisFileName=basename(__FILE__);
-
-if(isset($_GET["from"])==false){
-?>
-<form action="<?php echo $thisFileName;?>" method="get">
-From: <input type="text" name="from"><br>
-To: <input type="text" name="to"><br>
-<input type="submit">
-</form>
-<?php
-return;
+# 用拆分好的三藏数据 导出cs6段落编号
+require_once '../public/_pdo.php';
+require_once '../path.php';
+if ($argc < 3){
+	echo "无效的参数 ";
+	exit;
+}
+$from = (int)$argv[1];
+$to =(int)$argv[2];
+if($from<1){
+	$from = 1;
+}
+if($to>217){
+	$to = 217;
 }
 
-$from=$_GET["from"];
-$to=$_GET["to"];
+
 $filelist=array();
 $fileNums=0;
 $log="";
@@ -37,13 +28,12 @@ if(($handle=fopen("filelist.csv",'r'))!==FALSE){
 		$fileNums++;
 	}
 }
-if($to==0 || $to>=$fileNums) $to=$fileNums-1;
 
-$outputFile = fopen("xml/book_link.csv", "a") or die("Unable to open file!");
+$outputFile = fopen(_DIR_PALI_CSV_."/book_cs6_para.csv", "w") or die("Unable to open file!");
 $aBook = array();
 
-for($iFile=$from;$iFile<$to;$iFile++){
-	echo "<h2>$iFile</h2>";
+for($iFile=$from-1;$iFile<=$to-1;$iFile++){
+	echo "doing $iFile ";
 	$FileName=$filelist[$iFile][1].".htm";
 	$fileId=$filelist[$iFile][0];
 
@@ -53,14 +43,13 @@ for($iFile=$from;$iFile<$to;$iFile++){
 	$vriParNum=0;
 	$wordOrder=1;
 
-	$dirXmlBase="xml/";
+	$dirXmlBase=_DIR_PALI_CSV_."/";
 	$dirXml=$outputFileNameHead."/";
 
 	$currParNum="";
 
 	$xmlfile = $inputFileName;
-	echo "doing:".$xmlfile."<br>";
-	$log=$log."$from,$FileName,open\r\n";
+	# $log=$log."$from,$FileName,open\r\n";
 
 
 	// 打开文件并读取数据
@@ -71,19 +60,35 @@ for($iFile=$from;$iFile<$to;$iFile++){
 	if(($fp=fopen($dirXmlBase.$dirXml.$outputFileNameHead.".csv", "r"))!==FALSE){
 		while(($data=fgetcsv($fp,0,','))!==FALSE){
 			if($data[7]==".a."){
-				if(substr($data[4],0,4)=="para"){
+				if(stripos($data[4],"para")!==false){
 					if($bookid=stristr($data[4],"_")){
 						$bookid=substr($bookid,1);
-						$aBook["{$bookid}"]=1;
+						$paraString = stristr($data[4],"_",true);
+						$paraBegin = stripos($paraString,"para")+4;
+						$paraNum = explode("-",substr($paraString,$paraBegin));
+						$count++;
+						$output = array();
+						$output[] = substr($data[2],1);
+						$output[] = $data[3];
+						$output[] = $bookid;
+
+						foreach ($paraNum as $key => $value) {
+							# code...
+							$output[] = $value;
+						}
+						if(count($paraNum)==1){
+							$output[] = $paraNum[0];
+						}
+						fputcsv($outputFile,$output);
 					}
 				}
-			}			
+			}
 		}
 		fclose($fp);
-		echo "单词表load：".$dirXmlBase.$dirXml.$outputFileNameHead.".csv<br>";
+		echo "$count \n";
 	}
 	else{
-		echo "can not open csv file. filename=".$dirXmlBase.$dirXml.$outputFileNameHead.".csv";
+		echo "can not open csv file. filename=".$dirXmlBase.$dirXml.$outputFileNameHead.".csv \n";
 	}
 }
 
@@ -94,12 +99,8 @@ for($iFile=$from;$iFile<$to;$iFile++){
 	*/
 	fclose($outputFile);
 	
-	echo "count:".count($aBook)."<br>";
-	foreach($aBook as $x=>$value){
-		echo "{$x}<br>";
-	}
 
-	echo "<h2>齐活！功德无量！all done!</h2>";
+	echo "齐活！功德无量！all done! \n";
 
 ?>
 </body>
