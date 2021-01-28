0038acf852cbcb1ca0089af1ad9fceaab15fe0b4
paññābhinanda <panna.abhinanda@gmail.com>
2021-01-28 08:22:03 +0800

    首頁樣式重排 (分頁仍待調整)

--- app/pcdl/html_head.php
+++ app/pcdl/html_head.php
@@ -2,17 +2,15 @@
 require_once '../path.php';
 require_once '../public/load_lang.php';
 
-if(isset($_GET["language"])){
-	$currLanguage=$_GET["language"];
-	$_COOKIE["language"]=$currLanguage;
-}
-else{
-	if(isset($_COOKIE["language"])){
-		$currLanguage=$_COOKIE["language"];
-	}
-	else{
-		$currLanguage="en";
-		$_COOKIE["language"]=$currLanguage;
+if (isset($_GET["language"])) {
+	$currLanguage = $_GET["language"];
+	$_COOKIE["language"] = $currLanguage;
+} else {
+	if (isset($_COOKIE["language"])) {
+		$currLanguage = $_COOKIE["language"];
+	} else {
+		$currLanguage = "en";
+		$_COOKIE["language"] = $currLanguage;
 	}
 }
 
@@ -20,14 +18,15 @@ else{
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link type="text/css" rel="stylesheet" href="../pcdl/css/font.css"/>
-    <link type="text/css" rel="stylesheet" href="../pcdl/css/basic_style.css"/>
-    <link type="text/css" rel="stylesheet" href="../pcdl/css/style.css"/>
-    <link type="text/css" rel="stylesheet" href="../pcdl/css/color_day.css" id="colorchange" />
-    <link type="text/css" rel="stylesheet" href="../pcdl/css/style_mobile.css" media="screen and (max-width:800px)">
+	<link type="text/css" rel="stylesheet" href="../pcdl/css/font.css" />
+	<link type="text/css" rel="stylesheet" href="../pcdl/css/basic_style.css" />
+	<link type="text/css" rel="stylesheet" href="../pcdl/css/style.css" />
+	<link type="text/css" rel="stylesheet" href="../pcdl/css/color_day.css" id="colorchange" />
+	<link type="text/css" rel="stylesheet" href="../pcdl/css/style_mobile.css" media="screen and (max-width:800px)">
 	<link href="https://fonts.googleapis.com/css2?family=Padauk:wght@400;700&display=swap" rel="stylesheet">
 
 	<!-- generics -->
@@ -61,13 +60,13 @@ else{
 	<link rel="manifest" href="../public/images/favicon/site.webmanifest">
 	<link rel="mask-icon" href="../public/images/favicon/safari-pinned-tab.svg" color="#333333">
 
-    <title>wikipāḷi</title>
+	<title>wikipāḷi</title>
 
 	<script src="../public/js/jquery.js"></script>
 	<script src="../public/js/comm.js"></script>
 	<script src="../studio/js/fixedsticky.js"></script>
 	<script src="../guide/guide.js"></script>
-	<link type="text/css" rel="stylesheet" href="../guide/guide.css"/>
+	<link type="text/css" rel="stylesheet" href="../guide/guide.css" />
 
 	<script src="../ucenter/setting.js"></script>
 
@@ -75,87 +74,104 @@ else{
 	<script src="../public/js/mermaid.min.js"></script>
 
 	<script src="../public/js/notify.js"></script>
-    <link type="text/css" rel="stylesheet" href="../public/css/notify.css"/>
+	<link type="text/css" rel="stylesheet" href="../public/css/notify.css" />
 
 	<script src="../public/js/jquery-ui-1.12.1/jquery-ui.js"></script>
-	<link type="text/css" rel="stylesheet" href="../public/js/jquery-ui-1.12.1/jquery-ui.css"/>	
+	<link type="text/css" rel="stylesheet" href="../public/js/jquery-ui-1.12.1/jquery-ui.css" />
 
 	<script src="../term/term.js"></script>
 	<script src="../term/note.js"></script>
 	<script src="../term/term_popup.js"></script>
-	<link type="text/css" rel="stylesheet" href="../term/term.css"/>
+	<link type="text/css" rel="stylesheet" href="../term/term.css" />
 	<?php
-		if(isset($_GET["display"]) && $_GET["display"]=="para"){
-			echo '<link type="text/css" rel="stylesheet" href="../term/term_para.css"/>';
-		}
+	if (isset($_GET["display"]) && $_GET["display"] == "para") {
+		echo '<link type="text/css" rel="stylesheet" href="../term/term_para.css"/>';
+	}
 	?>
-		<script src="../channal/channal.js"></script>
+	<script src="../channal/channal.js"></script>
 	<script src="../term/popup_note.js"></script>
-	<link type="text/css" rel="stylesheet" href="../term/popup_note.css"/>
+	<link type="text/css" rel="stylesheet" href="../term/popup_note.css" />
 
 	<script src="../term/term_edit_dlg.js"></script>
-	<link type="text/css" rel="stylesheet" href="../term/term_edit_dlg.css"/>
+	<link type="text/css" rel="stylesheet" href="../term/term_edit_dlg.css" />
 	<script src="../uwbw/wbw_channal_list.js"></script>
 	<script src="../usent/historay.js"></script>
 	<script src="../term/pali_sim_sent.js"></script>
-	<link type="text/css" rel="stylesheet" href="../term/pali_sim_dlg.css"/>
+	<link type="text/css" rel="stylesheet" href="../term/pali_sim_dlg.css" />
 	<script src="../term/related_para.js"></script>
-	
+
 	<script src="../inline_dict/inline_dict.js"></script>
 
 	<script src="../widget/click_dropdown.js"></script>
-	<link type="text/css" rel="stylesheet" href="../widget/click_dropdown.css"/>
+	<link type="text/css" rel="stylesheet" href="../widget/click_dropdown.css" />
 
 	<script src="../public/charcode/coverter_my.js"></script>
 
-	<script >
-	<?php require_once '../public/load_lang_js.php';?>
+	<script>
+		<?php require_once '../public/load_lang_js.php'; ?>
 	</script>
 
 	<style>
-	.list_with_head{
-		display:flex;
-		margin: 3px 0;
-	}
-	.head_img{
-		display: inline-flex;
-    min-width: 3em;
-    height: 3em;
-    padding: 0 0px;
-    font-size: 60%;
-    background-color: gray;
-    color: white;
-    border-radius: 1.5em;
-    text-align: center;
-    justify-content: center;
-    margin: auto 2px;
-    line-height: 3em;
-	}
-	.card{
-		box-shadow: 0 0 10px rgba(0,0,0,0.15);
-		font-size: 1em;
-		line-height: 1.3;
-	}
-	.card>.title>a , .card>.title>a:link{
-		color: var(--main-color);
+		.list_with_head {
+			display: flex;
+			margin: 3px 0;
+		}
 
-	}
-	.card a:hover{
-		color: var(--tool-link-hover-color);
-	}
+		.head_img {
+			display: inline-flex;
+			min-width: 3em;
+			height: 3em;
+			padding: 0 0px;
+			font-size: 60%;
+			background-color: gray;
+			color: white;
+			border-radius: 1.5em;
+			text-align: center;
+			justify-content: center;
+			margin: auto 2px;
+			line-height: 3em;
+		}
 
-	.index_inner {
-	width:96vw;
-    max-width: 960px;
-    margin-left: auto;
-    margin-right: auto;
-}
-.card li{
-	white-space: normal;
-}
-.card code{
-	white-space: normal;
-}
+		.card {
+			border-radius: 20px;
+			box-shadow: 0 5px 7px rgba(0, 0, 0, 0.05);
+			margin: 10px 0;
+			background-color: white;
+			padding: 0.7rem;
+			display: flex;
+			position: relative;
+		}
 
+		.card>.title>a,
+		.card>.title>a:link {
+			color: var(--main-color);
+
+		}
+
+		.card a:hover {
+			color: var(--tool-link-hover-color);
+		}
+
+		.card_state {
+			position: absolute;
+			background-color: darkred;
+			color: white;
+			padding: 0 6px;
+			right: -5px;
+		}
+
+		.index_inner {
+
+			margin-left: auto;
+			margin-right: auto;
+		}
+
+		.card li {
+			white-space: normal;
+		}
+
+		.card code {
+			white-space: normal;
+		}
 	</style>
 </head>
--- app/term/new.php
+++ app/term/new.php
@@ -7,19 +7,17 @@ require_once '../public/load_lang.php';
 require_once '../public/function.php';
 
 global $PDO;
-PDO_Connect("sqlite:"._FILE_DB_TERM_);
+PDO_Connect("sqlite:" . _FILE_DB_TERM_);
 
 $query = "select word,meaning , owner from term where 1  order by create_time DESC limit 0,4";
 $Fetch = PDO_FetchAll($query);
 
-foreach($Fetch as $row){
-    echo '<div class="content_block">';
-    echo '<div class="card pd-10">';
-    echo '<div class="title" style="padding-bottom:5px;"><a href="../wiki/wiki.php?word='.$row["word"].'">'.$row["word"].'</a></div>';
-    echo '<div class="summary  style="padding-bottom:5px;">'.$row["meaning"].'</div>';
-    echo '<div class="author  style="padding-bottom:5px;">'.$row["owner"].'</div>';
-    echo '</div>';
+foreach ($Fetch as $row) {
+
+    echo '<div class="card">';
+    echo '<div class="title"><a href="../wiki/wiki.php?word=' . $row["word"] . '">' . $row["word"] . '</a></div>';
+    echo '<div class="summary">' . $row["meaning"] . '</div>';
+    echo '<div class="author">' . $row["owner"] . '</div>';
+
     echo '</div>';
 }
-
-?>
