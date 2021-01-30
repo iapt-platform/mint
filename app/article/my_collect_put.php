e0eda94d9a62bc637f99d0c757472d2bb47fcad8
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-01-30 13:09:24 +0630

    根据编辑行为加经验值

--- app/article/my_collect_put.php
+++ app/article/my_collect_put.php
@@ -3,6 +3,8 @@ require_once "../path.php";
 require_once "../public/_pdo.php";
 require_once '../public/function.php';
 require_once '../hostsetting/function.php';
+require_once "../ucenter/active.php";
+
 $respond=array("status"=>0,"message"=>"");
 PDO_Connect("sqlite:"._FILE_DB_USER_ARTICLE_);
 
