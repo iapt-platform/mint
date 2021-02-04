75f0310536b93a0d609eaedd5113ec39c70cf2b7
paññābhinanda <panna.abhinanda@gmail.com>
2021-02-04 15:58:29 +0800

    課程頁面UI更新

--- app/course/teacher_list.php
+++ app/course/teacher_list.php
@@ -7,23 +7,22 @@ require_once '../public/load_lang.php';
 require_once '../ucenter/function.php';
 
 global $PDO;
-PDO_Connect("sqlite:"._FILE_DB_COURSE_);
+PDO_Connect("sqlite:" . _FILE_DB_COURSE_);
 $query = "select teacher,count(*) as co from course where 1 group by teacher order by co DESC limit 0,4";
 $Fetch = PDO_FetchAll($query);
 
-foreach($Fetch as $value){
+foreach ($Fetch as $value) {
     echo '<div class="content_block">';
-    echo '<div class="card pd-10">';
-    echo '<div class="title" style="padding-bottom:5px;font-size:110%;font-weight:600;"><a href="../uhome/course.php?userid='.$value['teacher'].'">'.ucenter_getA($value['teacher']).'</a></div>';
-    
+    echo '<div class="teacher_photo"></div>';
+    echo '<div class="teacher_text">';
+    echo '<div class="title"><a href="../uhome/course.php?userid=' . $value['teacher'] . '">' . ucenter_getA($value['teacher']) . '</a></div>';
+    echo '<div class="teacher_intro">講師簡介</div>';
+    /*
     $query = "select id, title  from course where teacher = '{$value['teacher']}'  order by create_time DESC limit 0,5";
     $FetchTeacher = PDO_FetchAll($query);
-    foreach($FetchTeacher as $row){
-        echo '<div class="title" style="padding-bottom:5px;"><a href="../course/course.php?id='.$row["id"].'">'.$row["title"].'</a></div>';
-
-    }    
+    foreach ($FetchTeacher as $row) {
+        echo '<div class="title" style="padding-bottom:5px;"><a href="../course/course.php?id=' . $row["id"] . '">' . $row["title"] . '</a></div>';
+    }*/
     echo '</div>';
     echo '</div>';
 }
-
-?>
