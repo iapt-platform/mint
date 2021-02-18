674979021f879f47e215f95baaf515ff4349b8a5
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-02-18 19:06:38 +0630

    channel 添加后刷新页面 增加状态选择

--- app/channal/channal_add_dlg.js
+++ app/channal/channal_add_dlg.js
@@ -1,65 +1,70 @@
 var _channal_add_dlg_div;
 function channal_add_dlg_init(div) {
-  _channal_add_dlg_div = div;
-  let html = "";
-  html += "<div id='channal_add_dlg'>";
-  html += "<div >";
-  html += "<div >"+gLocal.gui.name+"</div>";
-  html += "<input type='input' id='channal_add_title' maxlength='32' placeholder='📝≤32'/>";
-  html += "</div>";
-  html += "<div>";
-  html += "</div>";
-  html +=
-    "<div style='display:flex;justify-content: space-between;padding-top: 1em;'>";
-  html += "<div>";
-  html += "<select id='channal_add_dlg_lang' name='lang'>";
-  html += "<option value='zh'>Chinese-中文</option>";
-  html += "<option value='en'>English-English</option>";
-  html += "<option value='my'>Mymarnese-မြန်မာ</option>";
-  html += "<option value='si'>Sinhalese-සිංහල</option>";
-  html += "</select>";
+	_channal_add_dlg_div = div;
+	let html = "";
+	html += "<div id='channal_add_dlg'>";
+	html += "<div >";
+	html += "<div >" + gLocal.gui.name + "</div>";
+	html += "<input type='input' id='channal_add_title' maxlength='32' placeholder='" + gLocal.gui.name + "'/>";
+	html += "</div>";
+	html += "<div>";
+	html += "</div>";
+	html += "<div style='display:flex;justify-content: space-between;padding-top: 1em;'>";
+	html += "<div>";
+	html += "<select id='channal_add_dlg_lang' name='lang'>";
+	html += "<option value='zh'>Chinese-中文</option>";
+	html += "<option value='en'>English-English</option>";
+	html += "<option value='my'>Mymarnese-မြန်မာ</option>";
+	html += "<option value='si'>Sinhalese-සිංහල</option>";
+	html += "</select>";
+	html += "<select id='channal_add_dlg_status' name='status'>";
+	html += "<option value='10'>" + gLocal.gui.private + "</option>";
+	html += "<option value='30'>" + gLocal.gui.public + "</option>";
+	html += "</select>";
 
-  html += "</div>";
-  html += "<div>";
-  html += "<button onclick='channal_add_cancel()'>"+gLocal.gui.cancel+"</button>";
-  html += "<button onclick='channal_add_new()'>"+gLocal.gui.new+"</button>";
-  html += "</div>";
-  html += "</div>";
-  html += "</div>";
+	html += "</div>";
+	html += "<div>";
+	html += "<button onclick='channal_add_cancel()'>" + gLocal.gui.cancel + "</button>";
+	html += "<button onclick='channal_add_new()'>" + gLocal.gui.new + "</button>";
+	html += "</div>";
+	html += "</div>";
+	html += "</div>";
 
-  $("#" + div).append(html);
+	$("#" + div).append(html);
 }
 
 function channal_add_dlg_show() {
-  $("#" + _channal_add_dlg_div).show();
+	$("#" + _channal_add_dlg_div).show();
 }
 function channal_add_dlg_hide() {
-  $("#" + _channal_add_dlg_div).hide();
+	$("#" + _channal_add_dlg_div).hide();
 }
 function channal_add_cancel() {
-  channal_add_dlg_hide();
-  $("#channal_add_title").val("");
+	channal_add_dlg_hide();
+	$("#channal_add_title").val("");
 }
 
 function channal_add_new() {
-  if ($("#channal_add_title").val() == "") {
-    alert("channal name is empty!");
-    return;
-  }
-  $.post(
-    "../channal/my_channal_put.php",
-    {
-      name: $("#channal_add_title").val(),
-      lang: $("#channal_add_dlg_lang").val(),
-    },
-    function (data) {
-      let error = JSON.parse(data);
-      if (error.status == 0) {
-        alert("ok");
-        channal_add_cancel();
-      } else {
-        alert(error.message);
-      }
-    }
-  );
+	if ($("#channal_add_title").val() == "") {
+		alert("channal name is empty!");
+		return;
+	}
+	$.post(
+		"../channal/my_channal_put.php",
+		{
+			name: $("#channal_add_title").val(),
+			lang: $("#channal_add_dlg_lang").val(),
+			status: $("#channal_add_dlg_status").val(),
+		},
+		function (data) {
+			let error = JSON.parse(data);
+			if (error.status == 0) {
+				alert("ok");
+				channal_add_cancel();
+				location.reload();
+			} else {
+				alert(error.message);
+			}
+		}
+	);
 }
--- app/channal/my_channal_index.php
+++ app/channal/my_channal_index.php
@@ -29,62 +29,35 @@ require_once '../studio/index_head.php';
 	<div class="index_inner" style="    margin-left: 18em;margin-top: 5em;">
 		<div id="channal_list"  class="file_list_block">
 
-		<div class="tool_bar">
-	<div>
-		<?php echo $_local->gui->channels; ?>
-	</div>
+			<div class="tool_bar">
+				<div>
+					<?php echo $_local->gui->channels; ?>
+				</div>
 
-	<div>
-		<span class="icon_btn_div">
-			<span class="icon_btn_tip"><?php echo "Add";?></span>
-			<button id="file_add" type="button" class="icon_btn" title=" "  onclick="channal_add_dlg_show()">
-				<svg class="icon">
-					<use xlink:href="../studio/svg/icon.svg#ic_add_circle"></use>
-				</svg>
-			</button>
-			<div id='channal_add_div' class="float_dlg"></div>
-		</span>
-		
-		<span class="icon_btn_div">				
-			<span class="icon_btn_tip"><?php echo $_local->gui->recycle_bin;?></span>
-			<button id="to_recycle" type="button" class="icon_btn" onclick="file_del()" title=" ">
-				<svg class="icon">
-					<use xlink:href="../studio/svg/icon.svg#ic_delete"></use>
-				</svg>
-			</button>
-		</span>	
-	</div>
-				
-</div>
+				<div>
+					<span class="icon_btn_div">
+						<span class="icon_btn_tip"><?php echo "Add";?></span>
+						<button id="file_add" type="button" class="icon_btn" title=" "  onclick="channal_add_dlg_show()">
+							<svg class="icon">
+								<use xlink:href="../studio/svg/icon.svg#ic_add_circle"></use>
+							</svg>
+						</button>
+						<div id='channal_add_div' class="float_dlg"></div>
+					</span>
+					
+					<span class="icon_btn_div">				
+						<span class="icon_btn_tip"><?php echo $_local->gui->recycle_bin;?></span>
+						<button id="to_recycle" type="button" class="icon_btn" onclick="file_del()" title=" ">
+							<svg class="icon">
+								<use xlink:href="../studio/svg/icon.svg#ic_delete"></use>
+							</svg>
+						</button>
+					</span>	
+				</div>
+					
+			</div>
 
-<div id="my_channal_list">
-<?php
-/*
-
-require_once "../path.php";
-require_once "../public/_pdo.php";
-require_once '../public/load_lang.php';
-require_once '../ucenter/function.php';
-
-global $PDO;
-PDO_Connect("sqlite:"._FILE_DB_CHANNAL_);
-$query = "SELECT * from channal where owner = '{$_COOKIE["userid"]}'   limit 0,100";
-$Fetch = PDO_FetchAll($query);
-
-foreach($Fetch as $row){
-    echo '<div class="file_list_row" style="padding:5px;">';
-
-    echo '<div class="pd-10"  style="width:2em;"><input  type="checkbox" /></div>';
-    echo '<div class="title" style="flex:3;padding-bottom:5px;"><a href="../course/my_course_index.php?course='.$row["id"].'">'.$row["name"].'</a></div>';
-    echo '<div class="summary"  style="flex:4;padding-bottom:5px;">'.$row["summary"].'</div>';
-    echo '<div class="summary"  style="flex:1;padding-bottom:5px;">'.$row["status"].'</div>';
-    echo '<div class="author"  style="flex:1;padding-bottom:5px;">'.$row["create_time"].'</div>';
-    
-    echo '</div>';
-}
-*/
-?>				
-</div>
+			<div id="my_channal_list">		</div>
 			
 		</div>
 		
