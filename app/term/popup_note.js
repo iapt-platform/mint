3b67f3f7e36758b1e05bcefb6809b5bc69bf805f
Bhikkhu-Kosalla <kosalla1987@gmail.com>
2021-04-24 07:08:24 +0630

    :art:

--- app/term/popup_note.js
+++ app/term/popup_note.js
@@ -7,10 +7,10 @@ function popup_init() {
 			const noteText = $(this).html();
 			$(this).html("");
 			if ($(this).offset().left < $(document.body).width() / 2) {
-				$(this).append('<div  class="popup_contence" style="left: -15px;">' + noteText + "</div>");
+				$(this).append('<div  class="popup_contence" style="left: -15px;">' + marked(noteText) + "</div>");
 				$(".popup_contence:after").css("left", "0");
 			} else {
-				$(this).append('<div  class="popup_contence" style="right: -15px;">' + noteText + "</div>");
+				$(this).append('<div  class="popup_contence" style="right: -15px;">' + marked(noteText) + "</div>");
 				$(".popup_contence:after").css("right", "0");
 			}
 			$(this).attr("init", "1");
