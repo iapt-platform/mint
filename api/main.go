924fddd4227b990ad35fd53c7ce15d17422534d6
visuddhinanda <visuddhinanda@gmail.com>
2021-07-09 19:08:02 +0630

    合并更改

--- api/main.go
+++ api/main.go
@@ -4,22 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-pg/pg/v10"
 	"github.com/iapt-platform/mint"
+	"fmt"
 )
 
 func main() {
-	opt, err := pg.ParseURL("postgres://postgres:@127.0.0.1:5432/mint?sslmode=disable")
+
+	opt, err := pg.ParseURL("postgres://postgres:@127.0.0.1:5433/mint?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Println("pg connectd")
 	db := pg.Connect(opt)
 	defer db.Close()
 
 	rt := gin.Default()
 
 	// TODO 在这里进行http mount
-	rt.GET("/demo/get", mint.GetDemo(db))
-	rt.POST("/demo/sign-in", mint.LoginDemo(db))
 
+	rt.GET("/demo/user/:id", mint.GetDemo(db))
+	//rt.POST("/demo/sign-in", mint.LoginDemo(db))
+	rt.POST("/demo/user",mint.PostDemo(db))
+	rt.PUT("/demo/user",mint.PutDemo(db))
+	rt.PATCH("/demo/user/:id",mint.PatchDemo(db))
+	rt.DELETE("/demo/user/:id",mint.DeleteDemo(db))
 	rt.Run()
 }
---
+++ db/migrations/2021-07-06-125510_create-users/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE users
+(
+id int,
+name varchar(255),
+emails varchar(255)
+)
