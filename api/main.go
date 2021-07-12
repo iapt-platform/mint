04dd8f14ef1b1621fedda0b542b654fa49427a19
visuddhinanda <visuddhinanda@gmail.com>
2021-07-12 20:48:39 +0630

    🚧 增加 course lesson api

--- api/main.go
+++ api/main.go
@@ -28,18 +28,29 @@ func main() {
 	rt.PATCH("/demo/user/:id",mint.PatchDemo(db))
 	rt.DELETE("/demo/user/:id",mint.DeleteDemo(db))
 
-
+	//根据id查询课程 
 	rt.GET("/api/course/:cid",mint.GetCourse(db))
+	//输入标题查询符合条件的课程 title% 
 	rt.GET("/api/course/title/:ctitle",mint.GetCourseByTitle(db))
+	//新建课程
 	rt.PUT("/api/course",mint.PutCourse(db)) 
-	//rt.POST /api/course/:cid/?data=data
-	//rt.DELETE /api/course/:cid
-/*
-	rt.GET /api/lesson/:lid
-	rt.GET /api/lessons/:cid
-	rt.PUT /api/lesson/?data=data
-	rt.POST /api/lesson/:lid/?data=data
-	rt.DELETE /api/lesson/:lid
-*/
+	//修改
+	rt.POST("/api/course",mint.PostCourse(db))//改
+	//删除
+	rt.DELETE("/api/course/:cid",mint.DeleteCourse(db)) 
+	//修改课程表里的课的数量
+	rt.POST("/api/course/lessonnum/:cid/:num",mint.PostLessonNumInCousrse(db))
+
+	//根据id查询课程
+	rt.GET("/api/lesson/:lid",mint.GetLesson(db))
+	//输入标题查询符合条件的课程 title% 
+	rt.GET("/api/lesson/title/:ltitle",mint.GetLessonByTitle(db))
+	//新建课
+	rt.PUT("/api/lesson",mint.PutLesson(db)) 
+	//修改
+	rt.POST("/api/lesson",mint.PostLesson(db))//改
+	//删除
+	rt.DELETE("/api/lesson/:lid",mint.DeleteLesson(db)) 
+
 	rt.Run()
 }
