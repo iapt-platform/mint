f14073260bee5b3d20b723ca22fbbaacc4dc6c66
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-08-31 18:54:22 +0630

    :art: 预览 ul缩进 增大边距 标题居中

--- app/article/my_article_edit.php
+++ app/article/my_article_edit.php
@@ -55,9 +55,27 @@ require_once '../studio/index_head.php';
 #preview_inner{
 	background-color: var(--bg-color);
 	color: var(--main-color);
-	padding: 0.5em;
+	padding: 1em;
 }
 
+#preview_inner ul{
+	display: block;
+    list-style-type: none;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+	text-indent: -1em;
+
+}
+#preview_inner p{
+	text-indent: 2em;
+}
+#preview_inner h3 {
+    font-size: 110%;
+	text-align: center;
+}
 	</style>
 
 	<?php
