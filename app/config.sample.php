920546c07e2bc5fe444fccee6e29e8109cbd4527
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-09-24 16:41:48 +0630

    建立casbin rbac 测试文件

--- app/config.sample.php
+++ app/config.sample.php
@@ -2,11 +2,14 @@
 // Require Composer's autoloader.
 require_once '../../vendor/autoload.php';
 
-// Require Composer's autoloader.
+
+/*
+电子邮件设置
+PHPMailer
+*/
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
 define("Email", ["SMTPDebug"=>SMTP::DEBUG_SERVER,//Enable verbose debug output DEBUG_OFF
 				 "Host"=>"smtp.gmail.com",//Set the SMTP server to send through
 				 "SMTPAuth"=>true,//Enable SMTP authentication
@@ -17,5 +20,29 @@ define("Email", ["SMTPDebug"=>SMTP::DEBUG_SERVER,//Enable verbose debug output D
 				 "From"=>"your@gmail.com",
 				 "Sender"=>"sender"
 				 ]);
+
+/*
+数据库设置
+*/
+define("Database",[
+	"type"=>"pgsql",
+	"port"=>5432,
+	"name"=>"mint",
+	"sslmode" => "disable",
+	"user" => "postgres",
+	"password" => ""
+]);
+
+/*
+Redis 设置，
+使用集群
+*/
+define("Redis",[
+	"hosts" => ["127.0.0.1:6376", "127.0.0.1:6377", "127.0.0.1:6378"],
+	"password" => "",
+	"db" => 0,
+	"prefix"=>"aaa://"
+]);
+				
 
 ?>
