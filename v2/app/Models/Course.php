81c5bb49d0c872f699dfa13740d20778a9d1a6ef
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-09-26 04:40:29 +0630

    启用 lavavel api

---
+++ v2/app/Models/Course.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+	protected $table = 'courses';
+	protected $fillable = [
+		'title'
+	];
+}
