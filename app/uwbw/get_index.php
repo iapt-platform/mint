831a6d3c34a6e9651661b5532d32d986e38b0760
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-11-16 15:02:29 +0630

    获取逐词解析列表

---
+++ app/uwbw/get_index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+输出某用户 wbw记录列表
+*/
+require_once "../path.php";
+require_once "../public/_pdo.php";
+require_once '../public/function.php';
+
+global $PDO;
+PDO_Connect(_FILE_DB_USER_WBW_);
+
+$query = "SELECT * from "._TABLE_USER_WBW_BLOCK_." where  owner = ? order by modify_time DESC";
+
+$result = PDO_FetchAll($query,array($_COOKIE["userid"]));
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
