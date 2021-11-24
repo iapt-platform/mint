e8eb56b3b8a4ead598282e33b61099f612158193
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-11-24 17:01:07 +0630

    避免配置文件用autoload 发送 email SMTPDebug 和 SMTPSecure写死

--- app/ucenter/invite.php
+++ app/ucenter/invite.php
@@ -62,7 +62,7 @@ if (PHP_SAPI == "cli") {
 			$mail->SMTPAuth   = Email["SMTPAuth"];                                   //Enable SMTP authentication
 			$mail->Username   = Email["Username"];                     //SMTP username
 			$mail->Password   = Email["Password"];                               //SMTP password
-			$mail->SMTPSecure = Email["SMTPSecure"];            //Enable implicit TLS encryption
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 			$mail->Port       = Email["Port"];                                    //TCP port to connect to 465; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 			$mail->CharSet =  'UTF-8';
 			$mail->Encoding = 'base64';
