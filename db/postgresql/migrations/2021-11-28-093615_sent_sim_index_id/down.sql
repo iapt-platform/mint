898d9b5f86e1836b9ef4221c31ad4bd09d97a95b
visuddhinanda <visuddhinanda@gmail.com>
2021-11-28 16:16:54 +0630

    pali sentence sim add index

---
+++ db/postgresql/migrations/2021-11-28-093615_sent_sim_index_id/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+
+DROP INDEX  IF EXISTS sent_sim_index_id ;
---
+++ db/postgresql/migrations/2021-11-28-093615_sent_sim_index_id/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+CREATE INDEX sent_sim_index_id ON sent_sim_index (sent_id);
