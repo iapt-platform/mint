e9fa08c7a7b71b005f31cdcdfebe739b442f2af2
visuddhinanda <visuddhinanda@gmail.com>
2021-12-03 04:20:24 +0630

    安装脚本加入语料库csv文件导入

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 date
-cd ./migaration
+php ../app/install/db_insert_templet_cli.php 1 217
+php ../app/install/db_update_toc.php 1 217 pali
+php ../app/install/db_update_toc.php 1 217 zh-hans
+php ../app/install/db_update_toc.php 1 217 zh-hant
+php ../app/install/db_insert_palitext.php 1 217
+php ../app/install/db_update_palitext.php 1 217
+php ../app/install/db_insert_bookword_from_csv_cli.php 1 217
+php ../app/install/db_insert_word_from_csv_cli.php 1 217
+php ../app/install/db_insert_wordindex_from_csv_cli.php
+php ../app/admin/word_index_weight_refresh.php 1 1
+
+cd /deploy/migaration
 php 20211202084900_init_pali_serieses.php
 php 20211125155600_word_statistics.php
 php 20211125155700_pali_sent_org.php
