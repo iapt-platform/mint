c295337606197ab27db750ef63999fbe6e7996ac
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-12-22 17:52:03 +0630

    table rename pali_sent_index->pali_sent_indexs

--- db/postgresql/migrations/2021-11-28-011108_pali_sent_index/up.sql
+++ db/postgresql/migrations/2021-11-28-011108_pali_sent_index/up.sql
@@ -1,6 +1,6 @@
 
 -- 表：pali_sent_index
-CREATE TABLE pali_sent_index (
+CREATE TABLE pali_sent_indexs (
 	id SERIAL PRIMARY KEY,
     book   INTEGER,
     para   INTEGER,
@@ -8,4 +8,4 @@ CREATE TABLE pali_sent_index (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX pali_sent_index_book ON pali_sent_index (book, para);
+CREATE INDEX pali_sent_index_book ON pali_sent_indexs (book, para);
