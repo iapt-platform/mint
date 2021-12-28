2e1a43e92a803dca26bfed34751d3917d564ee7f
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-12-29 05:15:24 +0630

    :construction: migrate book_title

---
+++ database/migrations/2021_12_28_223702_create_book_titles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTitlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_titles', function (Blueprint $table) {
+            $table->id();
+			$table->integer('book');
+            $table->integer('paragraph')->index();
+            $table->string('title',1024);
+            $table->timestamps();
+
+            $table->index(["book", "paragraph"]);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_titles');
+    }
+}
