95626ab1704a20b590bdfe1985ccb33b47cbb8a0
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-12-29 04:30:47 +0630

    :construction: add wbw_templates

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v2'],function(){
+	Route::apiResource('wbw_templates','WbwTemplateController');
+});
