e456b09d71ae3c4e38ba0a5ea660d166566d144e
visuddhinanda <visuddhinanda@users.noreply.github.com>
2021-12-30 11:53:20 +0630

    DROP EXTENSION "unaccent";

--- v1/db/postgresql/migrations/2021-12-03-150805_fts_texts/down.sql
+++ v1/db/postgresql/migrations/2021-12-03-150805_fts_texts/down.sql
@@ -2,12 +2,20 @@
 
 DROP TABLE fts_texts ;
 
-DROP TEXT SEARCH CONFIGURATION pali;
+-- 删除全文检索配置 pali
+DROP TEXT SEARCH CONFIGURATION  pali ;
 
-DROP TEXT SEARCH CONFIGURATION pali_unaccent;
+-- 删除全文检索配置 pali_unaccent 无标音符号版
+DROP TEXT SEARCH CONFIGURATION pali_unaccent ;
 
-DROP TEXT SEARCH DICTIONARY pali_stem;
+-- 删除巴利语词形转换字典
+DROP TEXT SEARCH DICTIONARY pali_stem ;
 
-DROP TEXT SEARCH DICTIONARY pali_stopwords;
+-- 删除巴利语停用词字典
+DROP TEXT SEARCH DICTIONARY pali_stopwords ;
 
-DROP FUNCTION query_pali;
+DROP EXTENSION "unaccent";
+
+-- 删除查询函数
+
+DROP  FUNCTION query_pali ;
