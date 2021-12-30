4cee640c878c0d81f4bfe3d436e280620c83823f
bhikkhu-kosalla-china <kosalla1987@126.com>
2021-12-30 20:54:15 +0800

    多进程安装

--- deploy/multi_process_install/install5.bat
+++ deploy/multi_process_install/install5.bat
@@ -16,10 +16,10 @@
 ::php ../../app/admin/word_index_weight_refresh.php 1 217
 
 php ../migaration/20211202084900_init_pali_serieses.php
-php ./migaration/20211125155600_word_statistics.php
-php ./migaration/20211125155700_pali_sent_org.php
-php ./migaration/20211125165700-pali_sent-upgrade.php
-php ./migaration/20211126220400-pali_sent_index-upgrade.php
+php ../migaration/20211125155600_word_statistics.php
+php ../migaration/20211125155700_pali_sent_org.php
+php ../migaration/20211125165700-pali_sent-upgrade.php
+php ../migaration/20211126220400-pali_sent_index-upgrade.php
 
 ::php ./migaration/20211127214800_sent_sim.php
 ::php ./migaration/20211127214900-sent_sim_index.php
