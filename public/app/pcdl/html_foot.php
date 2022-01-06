57ffe986a96f8d223e198f6e4ebb75ca63bc2ec5
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-06 07:30:18 +0630

    :sparkles: 脚注增加联系方式和反馈

--- public/app/pcdl/html_foot.php
+++ public/app/pcdl/html_foot.php
@@ -1,8 +1,33 @@
-<!--手机版导航-->
+<!---->
 
 <div class="foot_div">
+	<div class="">
+		<div>
+			<div>联系我们</div>
+			<div>
+				<div>地址：</div>
+				<div>景洪市</div>
+			</div>
+			<div>
+				<div>电子邮件：</div>
+				<div></div>
+			</div>
+			<div>
+				<div>微信公众号</div>
+				<div></div>
+			</div>
+			<div>
+				<div>github</div>
+				<div>https://www.github.com/iapt-platform/mint</div>
+			</div>		
+		</div>
+		<div>
+			<div>您的建议是我们宝贵的财富</div>
+			<div></div>
+		</div>
+	</div>
 	<div>wikipali @2020</div>
-	<div>Powered by PCD Suite</div>
+	<div>Powered by <a href='https://www.github.com/iapt-platform/mint'>PCD Suite</a></div>
 </div>
 
 <script>
