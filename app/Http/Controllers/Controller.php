4f6d667bd8a88c26ac52cadc2bda60f53318e061
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-06 07:28:55 +0630

    增加 sendResponse sendError

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public function sendResponse($result,$message){
+		$response = [
+			'ok' => true,
+			'data'=>$result,
+			'message'=> $message,
+		];
+		return response()->json($response,200);
+	}
+
+	public function sendError($error, $errorMessages = [], $code = 404){
+		$response = [
+			'ok' => false,
+			'data'=>$errorMessages,
+			'message'=> $error,
+		];
+		return response()->json($response,code);
+
+	}
 }
