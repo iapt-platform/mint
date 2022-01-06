734282c1ff5bba7b70c7b17954c0bc3d8078b647
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-06 13:43:36 +0630

    pg 表名为 book_titles

--- app/config.sample.php
+++ app/config.sample.php
@@ -95,7 +95,7 @@ define("_TABLE_RES_INDEX_","res_indexs");
 #pg
 define("_FILE_DB_PALITEXT_", _DB_ENGIN_.":host="._DB_HOST_.";port="._DB_PORT_.";dbname="._DB_NAME_.";user="._DB_USERNAME_.";password="._DB_PASSWORD_.";");
 define("_TABLE_PALI_TEXT_","pali_texts");
-define("_TABLE_PALI_BOOK_NAME_","books");
+define("_TABLE_PALI_BOOK_NAME_","book_titles");
 
 #单词表部分
 /*
