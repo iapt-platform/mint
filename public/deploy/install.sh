8a4acd5f5cde00008888620a60778e843775ac60
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-11 21:23:12 +0630

    word_index_weight_refresh.php 放在最后 因为太慢了

--- public/deploy/install.sh
+++ public/deploy/install.sh
@@ -11,7 +11,6 @@ php ../app/install/db_update_palitext_cli.php 1 217
 php ../app/install/db_insert_bookword_from_csv_cli.php 1 217
 php ../app/install/db_insert_word_from_csv_cli.php 1 217
 php ../app/install/db_insert_wordindex_from_csv_cli.php
-php ../app/admin/word_index_weight_refresh.php 1 217
 
 php ./migaration/20211202084900_init_pali_serieses.php
 php ./migaration/20211125155600_word_statistics.php
@@ -20,5 +19,7 @@ php ./migaration/20211125165700-pali_sent-upgrade.php
 php ./migaration/20211126220400-pali_sent_index-upgrade.php
 php ./migaration/20211127214800_sent_sim.php
 php ./migaration/20211127214900-sent_sim_index.php
+
+php ../app/admin/word_index_weight_refresh.php 1 217
 
 date
