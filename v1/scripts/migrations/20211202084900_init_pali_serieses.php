88a68d3cb8528af3c17fb037fdb0249579896803
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-12 00:46:32 +0630

    移到v1 修改安装脚本路径

--- v1/scripts/migrations/20211202084900_init_pali_serieses.php
+++ v1/scripts/migrations/20211202084900_init_pali_serieses.php
@@ -2,7 +2,7 @@
 /*
 导入巴利书名
 */
-require_once __DIR__."/../../app/config.php";
+require_once __DIR__."/../../../public/app/config.php";
 
 set_exception_handler(function($e){
 	fwrite(STDERR,"error-msg:".$e->getMessage().PHP_EOL);
