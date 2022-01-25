f554e9f659b7af8186dcde795da8578130d65669
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-01-25 15:49:33 +0630

    api 次数统计图用整数

--- app/log/showlog.js
+++ app/log/showlog.js
@@ -329,7 +329,7 @@ function chart_4(data){
 		tooltip: {
 			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
 			pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-				'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+				'<td style="padding:0"><b>{point.y} 次</b></td></tr>',
 			footerFormat: '</table>',
 			shared: true,
 			useHTML: true
