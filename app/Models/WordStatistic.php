44f9ccc2e65ae5d0ef99252f218618244c5e3ada
Bhikkhu-Kosalla <kosalla1987@gmail.com>
2022-01-26 08:59:48 +0630

    安装单词分析表

--- app/Models/WordStatistic.php
+++ app/Models/WordStatistic.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class WordStatistic extends Model
 {
     use HasFactory;
+	protected $fillable = ['bookid','word' , 'count', 'base' , 'end1' , 'end2','type', 'length'];
+
 }
