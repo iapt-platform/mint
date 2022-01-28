36030a6fe568d7b2072aa3db3c51b06f1eed61f7
Bhikkhu-Kosalla <kosalla1987@gmail.com>
2022-01-28 16:49:45 +0630

    :fire: note

--- database/migrations/2022_01_28_081954_create_user_dicts_table.php
+++ database/migrations/2022_01_28_081954_create_user_dicts_table.php
@@ -11,26 +11,6 @@ class CreateUserDictsTable extends Migration
      *
      * @return void
      */
-     /*
-    id SERIAL PRIMARY KEY,
-	word VARCHAR(1024)  NOT NULL , 
-	type VARCHAR(128), 
-	gramma VARCHAR(128), 
-	stem VARCHAR(1024), 
-	mean TEXT, 
-	note TEXT, 
-	factors VARCHAR(1024), 
-	factormean TEXT, 
-	status INTEGER  NOT NULL DEFAULT (1),
-	source VARCHAR(256), 
-	language VARCHAR(16)  NOT NULL DEFAULT ('en'), 
-	confidence INTEGER  NOT NULL DEFAULT (100), 
-	creator_id INTEGER   NOT NULL , 
-	ref_counter INTEGER DEFAULT (1),
-	create_time INTEGER  NOT NULL ,
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-     */
     public function up()
     {
         Schema::create('user_dicts', function (Blueprint $table) {
