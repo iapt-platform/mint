23d0ef63df0b2cc195c1c6ea3a17fb4f13cdbb4d
visuddhinanda@gmail.com <kosalla1987@gmail.com>
2022-05-25 11:48:35 +0630

    添加关联 tags tagid

--- app/Models/ProgressChapter.php
+++ app/Models/ProgressChapter.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class ProgressChapter extends Model
 {
     use HasFactory;
+	protected $fillable = ['book' , 'book', 'channel_id','lang'=>'en',
+                            'all_trans','public','progress',
+                            'title','created_at','updated_at'];
+    protected $primaryKey = 'uid';
+    public function tagid()
+    {
+        return $this->hasOne('App\Models\TagMap', 'anchor_id', 'uid'); //参数一:需要关联的子表类名,前面必须加上命名空间  参数二:子表关联父表的字段  参数三:父表关联子表的字段
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag','tag_maps','anchor_id','tag_id');
+    }
 }
