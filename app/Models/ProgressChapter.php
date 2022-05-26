d1828bef8ffaac0156a42ad7fec31d71bc3c61fb
visuddhinanda@gmail.com <kosalla1987@gmail.com>
2022-05-26 11:51:52 +0630

    指定 uid 转换为 string

--- app/Models/ProgressChapter.php
+++ app/Models/ProgressChapter.php
@@ -11,6 +11,9 @@ class ProgressChapter extends Model
 	protected $fillable = ['book' , 'book', 'channel_id','lang'=>'en',
                             'all_trans','public','progress',
                             'title','created_at','updated_at'];
+      protected $casts = [
+            'uid' => 'string'
+        ];
     protected $primaryKey = 'uid';
     public function tagid()
     {
@@ -20,5 +23,10 @@ class ProgressChapter extends Model
     public function tags()
     {
         return $this->belongsToMany('App\Models\Tag','tag_maps','anchor_id','tag_id');
+    }
+
+    public function channel()
+    {
+        return $this->hasOne('App\Models\Channel', 'uid', 'channel_id'); //参数一:需要关联的子表类名,前面必须加上命名空间  参数二:子表关联父表的字段  参数三:父表关联子表的字段
     }
 }
