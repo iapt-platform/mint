f156ae3c4b898a29cc2f8dce3af726bc0710cd31
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-09-27 06:47:30 +0630

    消除标题中的[

--- public/app/reader/get_para1.php
+++ public/app/reader/get_para1.php
@@ -181,7 +181,7 @@ if ($FetchParInfo) {
 			$sToc = str_replace("[","",$sToc);
 			$sToc = str_replace("]","",$sToc);
 			$sTocOutput .= $space . "- [{$sToc}](../article/index.php?view=chapter&book={$_book}&par={$value["paragraph"]}{$urlChannel}){$progress}\n";
-		}		
+		}
 	}
 	$sTocOutput .= "\n\n";
 
