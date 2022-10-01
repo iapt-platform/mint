9ba283e2853a451170bfc704edc04dc760e7bd03
Jeremy Zheng <jitang.zheng@gmail.com>
2022-10-01 08:39:27 +0530

    :memo: add typescript day-1 practice

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function demo_1(i: number, s: string): string {
+  console.log();
+  const str = `hello, react!(i = "${i}" s = "${s}")`;
+  return str;
+}
 
 function App() {
+  const demo_s = demo_1(123, "mint");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{demo_s}</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- documents/development/frontend/README.md
+++ documents/development/frontend/README.md
@@ -1,7 +1,29 @@
 # 前端培训文档
 
+## 安装依赖包
+
+```bash
+# offline
+tar xf node_modules.tar.xz
+# online
+yarn install
+```
+
+![dashboard.png](dashboard.png)
+
+## 开发模式启动 [dashboard](http://localhost:3000)
+
+```bash
+yarn start
+```
+
+## Typescript
+
 - [Typescript 第一天](ts-day-1/)
 - [Typescript 第二天](ts-day-2/)
+
+## React
+
 - [React 第一天](react-day-1/)
 - [React 第二天](react-day-2/)
 - [React 第三天](react-day-3/)
