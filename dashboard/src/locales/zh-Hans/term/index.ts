607f3c43988e14675b90d6f96a9ec4dc066fa7b2
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-10-10 20:08:38 +0630

    create i18n of term

---
+++ dashboard/src/locales/zh-Hans/term/index.ts
@@ -0,0 +1,12 @@
+const items = {
+  "term": "字典",
+  "term.fields.sn.label": "序号",
+  "term.fields.word.label": "词头",
+  "term.fields.tag.label": "标签",
+  "term.fields.channel.label": "所属版本",
+  "term.fields.meaning.label": "意思",
+  "term.fields.meaning2.label": "其他意思",
+  "term.fields.note.label": "注释",
+};
+
+export default items;
