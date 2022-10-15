158a7c43b72769c8e3d4b5790da7513ba791c4d7
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-10-15 18:16:57 +0630

    add logo

--- dashboard/src/components/studio/HeadBar.tsx
+++ dashboard/src/components/studio/HeadBar.tsx
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
-import { Col, Row,Input, Layout } from 'antd';
+import { Col, Row, Input, Layout } from "antd";
+
+import img_banner from "../../assets/studio/images/wikipali_banner.svg";
+
 const { Search } = Input;
 const { Header } = Layout;
+
 const onSearch = (value: string) => console.log(value);
 
 const Widget = () => {
-  return (
-	<Header className="header">
-		<Row>
-			<Col flex="100px">
-				<Link to="/">studio</Link>
-			</Col>
-			<Col flex="auto">
-				<Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
-			</Col>
-			<Col flex="200px">登录信息</Col>
-		</Row>		
-	</Header>
-
-  );
+	return (
+		<Header className="header">
+			<Row>
+				<Col flex="100px">
+					<Link to="/">
+						<img alt="code" src={img_banner} />
+					</Link>
+				</Col>
+				<Col flex="auto">
+					<Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+				</Col>
+				<Col flex="200px">登录信息</Col>
+			</Row>
+		</Header>
+	);
 };
 
 export default Widget;
