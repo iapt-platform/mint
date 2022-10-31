0a0be0102143af32204a8a85b1ce5c20b24443e2
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-10-31 15:26:30 +0630

    :fire: import button

--- dashboard/src/components/nut/Home.tsx
+++ dashboard/src/components/nut/Home.tsx
@@ -1,20 +1,24 @@
+import ReactMarkdown from "react-markdown";
 import code_png from "../../assets/nut/code.png";
 import MarkdownForm from "./MarkdownForm";
 import MarkdownShow from "./MarkdownShow";
 
 const Widget = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <br />
-      <MarkdownShow body="- Hello, **《mint》**!" />
-      <br />
-      <h3>Form</h3>
-      <MarkdownForm />
-      <br />
-      <img alt="code" src={code_png} />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home</h1>
+			<br />
+			<MarkdownShow body="- Hello, **《mint》**!" />
+			<br />
+			<h3>Form</h3>
+			<MarkdownForm />
+			<br />
+			<img alt="code" src={code_png} />
+			<div>
+				<ReactMarkdown>*This* is text with `quote`</ReactMarkdown>
+			</div>
+		</div>
+	);
 };
 
 export default Widget;
