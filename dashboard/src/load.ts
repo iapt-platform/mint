2c1446bea087c214a536f278c6d1ffccb153ff9f
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-10-31 15:01:16 +0630

    get current user profile

--- dashboard/src/load.ts
+++ dashboard/src/load.ts
@@ -1,19 +1,38 @@
-import { Empty } from "google-protobuf/google/protobuf/empty_pb";
-import { Duration } from "google-protobuf/google/protobuf/duration_pb";
+//import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+//import { Duration } from "google-protobuf/google/protobuf/duration_pb";
 
-import { DURATION, get as getToken, signIn } from "./reducers/current-user";
-import { refresh as refreshLayout } from "./reducers/layout";
-import { GRPC_HOST, get as httpGet, grpc_metadata } from "./request";
+import { get as getToken, IUser, signIn } from "./reducers/current-user";
+//import { DURATION } from "./reducers/current-user";
+import { ISite, refresh as refreshLayout } from "./reducers/layout";
+import { get } from "./request";
+//import { GRPC_HOST,  grpc_metadata } from "./request";
 import store from "./store";
 
+interface IUserResponse {
+	ok: boolean;
+	message: string;
+	data: IUser;
+}
 const init = () => {
-  // TODO ajax get site information, SEE reducers/layout/ISite
-  // store.dispatch(refreshLayout(it));
-
-  if (getToken()) {
-    // TODO get current user profile & new token, SEE reducers/current-user/IUser
-    // store.dispatch(signIn([user, token]));
-  }
+	console.log("onload");
+	// ajax get site information, SEE reducers/layout/ISite
+	get("/v2/siteinfo/en").then((json) => {
+		store.dispatch(refreshLayout(json as ISite));
+	});
+	const token = getToken();
+	if (token) {
+		// get current user profile & new token, SEE reducers/current-user/IUser
+		get("/v2/auth/current").then((json) => {
+			const user = json as IUserResponse;
+			if (user.ok) {
+				store.dispatch(signIn([user.data, token ? token : ""]));
+			} else {
+				console.error(user.message);
+			}
+		});
+	} else {
+		console.log("no token");
+	}
 };
 
 export default init;
