c96ec97ff8225e7e5bbc3985a3f9c64fe6c46bdb
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-10-31 15:21:16 +0630

    fluent create

--- dashboard-fluent/src/locales/index.ts
+++ dashboard-fluent/src/locales/index.ts
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+
 import "moment/locale/zh-cn";
 import "moment/locale/zh-tw";
 import "moment/locale/es";
@@ -13,48 +14,45 @@ import zhHant from "./zh-Hant";
 
 const KEY = "locale";
 
-export const DEFAULT: string =
-  process.env.REACT_APP_DEFAULT_LOCALE || "zh-Hans";
-export const LANGUAGES: string[] = process.env.REACT_APP_LANGUAGES?.split(
-  ","
-) || ["en-US", "zh-Hans"];
+export const DEFAULT: string = process.env.REACT_APP_DEFAULT_LOCALE || "zh-Hans";
+export const LANGUAGES: string[] = process.env.REACT_APP_LANGUAGES?.split(",") || ["en-US", "zh-Hans"];
 
 export const get = (): string => {
-  return localStorage.getItem(KEY) || Cookies.get(KEY) || DEFAULT;
+	return localStorage.getItem(KEY) || Cookies.get(KEY) || DEFAULT;
 };
 
 export const set = (lang: string, reload: boolean) => {
-  Cookies.set(KEY, lang);
-  localStorage.setItem(KEY, lang);
-  if (reload) {
-    window.location.reload();
-  }
+	Cookies.set(KEY, lang);
+	localStorage.setItem(KEY, lang);
+	if (reload) {
+		window.location.reload();
+	}
 };
 
 export const remove = () => {
-  Cookies.remove(KEY);
-  localStorage.removeItem(KEY);
+	Cookies.remove(KEY);
+	localStorage.removeItem(KEY);
 };
 
 interface ILocale {
-  messages: Record<string, string>;
+	messages: Record<string, string>;
 }
 
 const messages = (lang: string): ILocale => {
-  switch (lang) {
-    case "en-US":
-      return {
-        messages: { ...enUS, ...languages },
-      };
-    case "zh-Hant":
-      return {
-        messages: { ...zhHant, ...languages },
-      };
-    default:
-      return {
-        messages: { ...zhHans, ...languages },
-      };
-  }
+	switch (lang) {
+		case "en-US":
+			return {
+				messages: { ...enUS, ...languages },
+			};
+		case "zh-Hant":
+			return {
+				messages: { ...zhHant, ...languages },
+			};
+		default:
+			return {
+				messages: { ...zhHans, ...languages },
+			};
+	}
 };
 
 export default messages;
--- dashboard-fluent/src/pages/Home.tsx
+++ dashboard-fluent/src/pages/Home.tsx
@@ -1,20 +1,23 @@
 import { PrimaryButton } from "@fluentui/react";
+import HeadBar from "../components/library/HeadBar";
 
 const Widget = () => {
-  return (
-    <div>
-      <h1>home</h1>
-      <div>
-        <PrimaryButton
-          onClick={() => {
-            alert("aaa");
-          }}
-        >
-          Demo
-        </PrimaryButton>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<HeadBar />
+			</div>
+			<div>
+				<PrimaryButton
+					onClick={() => {
+						alert("aaa");
+					}}
+				>
+					Demo
+				</PrimaryButton>
+			</div>
+		</div>
+	);
 };
 
 export default Widget;
