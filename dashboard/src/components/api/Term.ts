7c16a1d49be4d63ed5887c390b98674a9dded0ce
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-04 08:53:39 +0630

    :construction: create

---
+++ dashboard/src/components/api/Term.ts
@@ -0,0 +1,35 @@
+export interface ITermDataRequest {
+	id: number;
+	word: string;
+	tag: string;
+	meaning: string;
+	other_meaning: string;
+	note: string;
+	channal: string;
+	language: string;
+}
+export interface ITermDataResponse {
+	id: number;
+	word: string;
+	tag: string;
+	meaning: string;
+	other_meaning: string;
+	note: string;
+	channal: string;
+	language: string;
+	created_at: string;
+	updated_at: string;
+}
+export interface ITermResponse {
+	ok: boolean;
+	message: string;
+	data: ITermDataResponse;
+}
+export interface ITermListResponse {
+	ok: boolean;
+	message: string;
+	data: {
+		rows: ITermDataResponse[];
+		count: number;
+	};
+}
