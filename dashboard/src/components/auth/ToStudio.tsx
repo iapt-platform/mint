48d2b51341fdde83f950fd73cef3353f84343e3f
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-06 11:42:54 +0630

    :fire: useEffect

--- dashboard/src/components/auth/ToStudio.tsx
+++ dashboard/src/components/auth/ToStudio.tsx
@@ -1,22 +1,23 @@
 import { Button } from "antd";
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../hooks";
 import { currentUser as _currentUser } from "../../reducers/current-user";
 
 const Widget = () => {
-	const [userName, setUserName] = useState("");
 	const user = useAppSelector(_currentUser);
-	useEffect(() => {
-		setUserName(user ? user.realName : "");
-	}, [user]);
-	return (
-		<>
-			<Link to={`/studio/${userName}/home`}>
-				<Button type="primary">藏经阁</Button>
-			</Link>
-		</>
-	);
+
+	if (typeof user !== "undefined") {
+		return (
+			<>
+				<Link to={`/studio/${user.realName}/home`}>
+					<Button type="primary">藏经阁</Button>
+				</Link>
+			</>
+		);
+	} else {
+		return <></>;
+	}
 };
 
 export default Widget;
