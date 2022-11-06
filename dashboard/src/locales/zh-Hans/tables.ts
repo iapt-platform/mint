780df8017b596d331f88b38de2f3d6e1da37e4e7
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-06 21:27:10 +0630

    :globe_with_meridians: add role

--- dashboard/src/locales/zh-Hans/tables.ts
+++ dashboard/src/locales/zh-Hans/tables.ts
@@ -5,6 +5,11 @@ const items = {
 	"tables.publicity.public.bylink": "链接公开",
 	"tables.publicity.public": "公开",
 	"tables.publicity.public.edit": "公开可编辑",
+	"tables.role.all": "全部",
+	"tables.role.owner": "拥有者",
+	"tables.role.manager": "管理员",
+	"tables.role.editor": "编辑",
+	"tables.role.member": "成员",
 };
 
 export default items;
