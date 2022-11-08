bc3b373d5acac7ceffd7fe9b8ccef3f50be477a2
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-08 20:49:32 +0630

    add primaryKey

--- app/Models/UserDict.php
+++ app/Models/UserDict.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class UserDict extends Model
 {
     use HasFactory;
-	protected $casts = [
-		'id' => 'string'
-	];
+    protected $primaryKey = 'id';
+    public  $incrementing = false;
 }
