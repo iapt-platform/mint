42363a7d28b83f3fdb6942979de67fa355910a09
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-08 09:58:25 +0630

    test 模版

--- dashboard/src/components/nut/Home.tsx
+++ dashboard/src/components/nut/Home.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import code_png from "../../assets/nut/code.png";
+import MdView from "../template/MdView";
 import MarkdownForm from "./MarkdownForm";
 import MarkdownShow from "./MarkdownShow";
 
@@ -7,6 +8,8 @@ const Widget = () => {
 	return (
 		<div>
 			<h1>Home</h1>
+			<h2>mardown test</h2>
+			<MdView html="<h1 name='h1'>hello<MdTpl name='term'/></h1>" />
 			<br />
 			<MarkdownShow body="- Hello, **《mint》**!" />
 			<br />
