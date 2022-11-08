cc6c101cd1342efd005225c9a7fb5d559c0f6387
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-08 20:41:31 +0630

    add $primaryKey

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Channel extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'uid';
+    protected $casts = [
+        'uid' => 'string'
+    ];
+    public  $incrementing = false;
 }
