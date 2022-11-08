5182a83001c2a89501fa95363795debeb32ad18b
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-08 20:50:50 +0630

    :memo: invalid->exists

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -46,7 +46,7 @@ return [
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
     'enum' => 'The selected :attribute is invalid.',
-    'exists' => 'The selected :attribute is invalid.',
+    'exists' => 'The selected :attribute is exists.',
     'file' => 'The :attribute must be a file.',
     'filled' => 'The :attribute field must have a value.',
     'gt' => [
