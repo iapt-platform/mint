18d65901d67339ac843f764f535691b5a2e4d725
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-10 19:48:42 +0630

    :art: use antd result

--- dashboard/src/pages/nut/not-found.tsx
+++ dashboard/src/pages/nut/not-found.tsx
@@ -1,10 +1,19 @@
-const titleStyle = {
-  fontSize: "48px",
-  color: "red",
-};
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
 const Widget = () => {
-  return <div style={titleStyle}>forbidden</div>;
+	return (
+		<Result
+			status="404"
+			title="404"
+			subTitle="Sorry, the page you visited does not exist."
+			extra={
+				<Link to="/">
+					<Button type="primary">Back Home</Button>
+				</Link>
+			}
+		/>
+	);
 };
 
 export default Widget;
