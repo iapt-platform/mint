f7ca805c465a8ad2be9c9450ad0147bee15d4995
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-12 20:45:36 +0630

    overlay -> menu

--- dashboard/src/components/general/UiLangSelect.tsx
+++ dashboard/src/components/general/UiLangSelect.tsx
@@ -1,35 +1,33 @@
-import { Dropdown, Menu, Button } from "antd";
+import { Dropdown, Button } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 
-const onClick: MenuProps["onClick"] = (e) => {
-	console.log("click ", e);
-};
-
-const menu = (
-	<Menu
-		onClick={onClick}
-		items={[
-			{
-				key: "en",
-				label: "English",
-			},
-			{
-				key: "zh-Hans",
-				label: "简体中文",
-			},
-			{
-				key: "zh-Hant",
-				label: "繁体中文",
-			},
-		]}
-	/>
-);
+const items: MenuProps["items"] = [
+	{
+		key: "en",
+		label: "English",
+	},
+	{
+		key: "zh-Hans",
+		label: "简体中文",
+	},
+	{
+		key: "zh-Hant",
+		label: "繁体中文",
+	},
+];
 const Widget = () => {
 	// TODO
 	return (
-		<Dropdown overlay={menu} placement="bottomRight">
-			<Button ghost icon={<GlobalOutlined />}>
+		<Dropdown menu={{ items }} placement="bottomRight">
+			<Button
+				ghost
+				icon={<GlobalOutlined />}
+				onClick={(e) => {
+					console.log("click ", e);
+					e.preventDefault();
+				}}
+			>
 				简体中文
 			</Button>
 		</Dropdown>
