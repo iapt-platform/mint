12a02c9789f7ec4ba09e2cd202d8aeb4292e627c
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-16 21:07:07 +0630

    add buttons.read

--- dashboard/src/locales/zh-Hans/buttons.ts
+++ dashboard/src/locales/zh-Hans/buttons.ts
@@ -1,12 +1,13 @@
 const items = {
-	"buttons.submit": "提交",
-	"buttons.create": "新建",
-	"buttons.edit": "编辑",
-	"buttons.delete": "删除",
-	"buttons.delete.all": "批量删除",
-	"buttons.selected": "已经选择",
-	"buttons.unselect": "取消选择",
-	"buttons.option": "操作",
+  "buttons.submit": "提交",
+  "buttons.create": "新建",
+  "buttons.edit": "编辑",
+  "buttons.read": "阅读",
+  "buttons.delete": "删除",
+  "buttons.delete.all": "批量删除",
+  "buttons.selected": "已经选择",
+  "buttons.unselect": "取消选择",
+  "buttons.option": "操作",
 };
 
 export default items;
