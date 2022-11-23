18ecc73703f7918a23c1a264882c920297450962
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-23 09:21:37 +0630

    :art: formatting

--- dashboard/src/components/general/UiLangSelect.tsx
+++ dashboard/src/components/general/UiLangSelect.tsx
@@ -3,35 +3,34 @@ import { GlobalOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 
 const items: MenuProps["items"] = [
-	{
-		key: "en",
-		label: "English",
-	},
-	{
-		key: "zh-Hans",
-		label: "简体中文",
-	},
-	{
-		key: "zh-Hant",
-		label: "繁体中文",
-	},
+  {
+    key: "en",
+    label: "English",
+  },
+  {
+    key: "zh-Hans",
+    label: "简体中文",
+  },
+  {
+    key: "zh-Hant",
+    label: "繁体中文",
+  },
 ];
 const Widget = () => {
-	// TODO
-	return (
-		<Dropdown menu={{ items }} placement="bottomRight">
-			<Button
-				ghost
-				icon={<GlobalOutlined />}
-				onClick={(e) => {
-					console.log("click ", e);
-					e.preventDefault();
-				}}
-			>
-				简体中文
-			</Button>
-		</Dropdown>
-	);
+  // TODO
+  return (
+    <Dropdown menu={{ items }} placement="bottomRight">
+      <Button
+        ghost
+        icon={<GlobalOutlined />}
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        简体中文
+      </Button>
+    </Dropdown>
+  );
 };
 
 export default Widget;
