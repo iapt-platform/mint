fc500810b69f1f4349d32bef2aa6d4123d4e18fb
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-23 09:12:48 +0630

    :sparkles: add setting

--- dashboard/src/store.ts
+++ dashboard/src/store.ts
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import currentUserReducer from "./reducers/current-user";
 import layoutReducer from "./reducers/layout";
 import openArticleReducer from "./reducers/open-article";
+import settingReducer from "./reducers/setting";
 
 const store = configureStore({
   reducer: {
     layout: layoutReducer,
     currentUser: currentUserReducer,
     openArticle: openArticleReducer,
+    setting: settingReducer,
   },
 });
 
