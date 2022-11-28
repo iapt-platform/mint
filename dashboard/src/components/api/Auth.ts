83d551ea2a70be1c16474d5d1ec48f5e51450109
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-11-28 18:02:12 +0630

    add type Role

--- dashboard/src/components/api/Auth.ts
+++ dashboard/src/components/api/Auth.ts
@@ -1,14 +1,16 @@
+export type Role = "owner" | "manager" | "editor" | "member";
+
 export interface IUserApiResponse {
-	id: string;
-	userName: string;
-	nickName: string;
-	avatar: string;
+  id: string;
+  userName: string;
+  nickName: string;
+  avatar: string;
 }
 
 export interface IStudioApiResponse {
-	id: string;
-	nickName: string;
-	studioName: string;
-	avatar: string;
-	owner: IUserApiResponse;
+  id: string;
+  nickName: string;
+  studioName: string;
+  avatar: string;
+  owner: IUserApiResponse;
 }
