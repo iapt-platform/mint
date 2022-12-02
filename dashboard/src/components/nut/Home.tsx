80f57af6903b45d84060d21e75035f746af06b64
Jeremy Zheng <jitang.zheng@gmail.com>
2022-12-03 05:09:15 +0530

    :construction: merge font files & add font demo

--- dashboard/src/components/nut/Home.tsx
+++ dashboard/src/components/nut/Home.tsx
@@ -2,23 +2,26 @@ import ReactMarkdown from "react-markdown";
 import code_png from "../../assets/nut/code.png";
 import MarkdownForm from "./MarkdownForm";
 import MarkdownShow from "./MarkdownShow";
+import FontBox from "./FontBox";
 
 const Widget = () => {
-	return (
-		<div>
-			<h1>Home</h1>
-			<br />
-			<MarkdownShow body="- Hello, **《mint》**!" />
-			<br />
-			<h3>Form</h3>
-			<MarkdownForm />
-			<br />
-			<img alt="code" src={code_png} />
-			<div>
-				<ReactMarkdown>*This* is text with `quote`</ReactMarkdown>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Home</h1>
+      <br />
+      <FontBox />
+      <br />
+      <MarkdownShow body="- Hello, **《mint》**!" />
+      <br />
+      <h3>Form</h3>
+      <MarkdownForm />
+      <br />
+      <img alt="code" src={code_png} />
+      <div>
+        <ReactMarkdown>*This* is text with `quote`</ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
