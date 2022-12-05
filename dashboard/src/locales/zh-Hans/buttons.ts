9a4d7b636c96eaec6f42970bde96e1a91b541736
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-12-05 22:23:22 +0630

    :globe_with_meridians: add save , cancel

--- dashboard/src/locales/zh-Hans/buttons.ts
+++ dashboard/src/locales/zh-Hans/buttons.ts
@@ -6,8 +6,12 @@ const items = {
   "buttons.delete": "删除",
   "buttons.delete.all": "批量删除",
   "buttons.selected": "已经选择",
+  "buttons.select": "选择",
   "buttons.unselect": "取消选择",
   "buttons.option": "操作",
+  "buttons.save": "保存",
+  "buttons.save.publish": "保存并公开",
+  "buttons.cancel": "取消",
 };
 
 export default items;
