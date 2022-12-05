10ef8c49f7b9baed6aba427adcc0ece4d88651be
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-12-05 22:22:07 +0630

    :globe_with_meridians: add role

---
+++ dashboard/src/locales/zh-Hans/auth/index.ts
@@ -0,0 +1,9 @@
+const items = {
+  "auth.role.label": "角色",
+  "auth.role.owner": "拥有者",
+  "auth.role.manager": "管理员",
+  "auth.role.editor": "编辑者",
+  "auth.role.member": "成员",
+};
+
+export default items;
