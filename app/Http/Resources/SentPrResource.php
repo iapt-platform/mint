2a078c4bc58241b2445e72a5324b81db0252a62a
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-12-07 20:33:32 +0630

    :sparkles: create

---
+++ app/Http/Resources/SentPrResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Api\MdRender;
+use App\Http\Api\StudioApi;
+use App\Http\Api\ChannelApi;
+
+class SentPrResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //判断权限
+        $user = AuthApi::current($request);
+        $role = 'reader';
+        if($user && $user["user_uid"] === $this->editor_uid ){
+            $role = 'owner';
+        }
+        return [
+            "id"=>$this->id,
+            "book"=> $this->book_id,
+            "paragraph"=> $this->paragraph,
+            "word_start"=> $this->word_start,
+            "word_end"=> $this->word_end,
+            "editor"=> StudioApi::getById($this->editor_uid),
+            "channel"=> ChannelApi::getById($this->channel_uid),
+            "content"=>$this->content,
+            "html"=> MdRender::render($this->content,$this->channel_uid),
+            "role"=>$role,
+            "created_at"=> $this->created_at,
+            "updated_at"=> $this->updated_at,
+        ];
+    }
+}
