98be42a5d5b636e4c64121ebaaebf4a60c4f52bb
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-12-07 14:33:03 +0630

    add UnLock icon

--- dashboard/src/assets/icon/index.tsx
+++ dashboard/src/assets/icon/index.tsx
@@ -53,6 +53,18 @@ const LockSvg = () => (
     <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
   </svg>
 );
+
+const UnLockSvg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2zM3 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H3z" />
+  </svg>
+);
 export const DictIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={DictSvg} {...props} />
 );
@@ -66,4 +78,8 @@ export const SuggestionIcon = (props: Partial<CustomIconComponentProps>) => (
 
 export const LockIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={LockSvg} {...props} />
+);
+
+export const UnLockIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={UnLockSvg} {...props} />
 );
