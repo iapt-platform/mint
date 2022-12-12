d7383927557f82f6e4829e32d0025f4e43789e53
visuddhinanda <visuddhinanda@users.noreply.github.com>
2022-12-12 21:41:12 +0630

    标点符号的显示

--- dashboard/src/components/template/Wbw/WbwPali.tsx
+++ dashboard/src/components/template/Wbw/WbwPali.tsx
@@ -6,6 +6,7 @@ import WbwDetail from "./WbwDetail";
 import { IWbw } from "./WbwWord";
 import { bookMarkColor } from "./WbwDetailBookMark";
 import "./wbw.css";
+import { PaliReal } from "../../../utils";
 
 interface IWidget {
   data: IWbw;
@@ -57,26 +58,50 @@ const Widget = ({ data, onSave }: IWidget) => {
     <></>
   );
   const classPali = data.style?.value === "note" ? "wbw_note" : "pali";
-  return (
-    <div className="pali_shell">
-      <Popover
-        content={wbwDetail}
-        placement="bottom"
-        trigger="click"
-        open={open}
-        onOpenChange={handleClickChange}
-      >
-        <span
-          className={classPali}
-          style={{ backgroundColor: paliColor, padding: 4, borderRadius: 5 }}
-        >
-          {data.word.value}
-        </span>
-      </Popover>
-      {noteIcon}
-      {bookMarkIcon}
-    </div>
+  let padding: string;
+  if (typeof data.real !== "undefined" && PaliReal(data.real.value) !== "") {
+    padding = "4px";
+  } else {
+    padding = "4px 0";
+  }
+  const paliWord = (
+    <span
+      className={classPali}
+      style={{
+        backgroundColor: paliColor,
+        padding: padding,
+        borderRadius: 5,
+      }}
+    >
+      {data.word.value}
+    </span>
   );
+
+  if (typeof data.real !== "undefined" && PaliReal(data.real.value) !== "") {
+    //非标点符号
+    return (
+      <div className="pali_shell">
+        <Popover
+          content={wbwDetail}
+          placement="bottom"
+          trigger="click"
+          open={open}
+          onOpenChange={handleClickChange}
+        >
+          {paliWord}
+        </Popover>
+        {noteIcon}
+        {bookMarkIcon}
+      </div>
+    );
+  } else {
+    //标点符号
+    return (
+      <div className="pali_shell" style={{ cursor: "unset" }}>
+        {paliWord}
+      </div>
+    );
+  }
 };
 
 export default Widget;
