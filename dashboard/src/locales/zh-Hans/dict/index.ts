53d93dd591d6ce938619cafb9862fb72c809ae93
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2022-12-22 10:15:28 +0630

    add null

--- dashboard/src/locales/zh-Hans/dict/index.ts
+++ dashboard/src/locales/zh-Hans/dict/index.ts
@@ -132,6 +132,8 @@ const items = {
   "dict.fields.type.un.short.label": "连音",
   "dict.fields.type.none.label": "无",
   "dict.fields.type.none.short.label": "",
+  "dict.fields.type.null.label": "空",
+  "dict.fields.type.null.short.label": "",
   "dict.fields.type.?.label": "?",
   "dict.fields.type.?.short.label": "?",
 };
