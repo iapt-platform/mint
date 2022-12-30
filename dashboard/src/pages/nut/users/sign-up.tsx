c5025c3311c50eef0972de5ac961e865267967be
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2022-12-30 10:07:00 +0630

    :fire: 去掉用户登录下面的多余链接

--- dashboard/src/pages/nut/users/sign-up.tsx
+++ dashboard/src/pages/nut/users/sign-up.tsx
@@ -1,12 +1,20 @@
+import { Card } from "antd";
+import { useIntl } from "react-intl";
 import SharedLinks from "../../../components/nut/users/NonSignInSharedLinks";
 
 const Widget = () => {
+  const intl = useIntl();
+
   return (
-    <div>
+    <Card
+      title={intl.formatMessage({
+        id: "nut.users.sign-in.title",
+      })}
+    >
       sign up
       <br />
       <SharedLinks />
-    </div>
+    </Card>
   );
 };
 
