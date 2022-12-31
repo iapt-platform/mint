fbbe183712c7452fd5fb8bf7b8a39555dd801edb
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2022-12-31 08:24:34 +0630

    add forgot-password

--- dashboard/src/components/nut/users/NonSignInSharedLinks.tsx
+++ dashboard/src/components/nut/users/NonSignInSharedLinks.tsx
@@ -1,6 +1,6 @@
 import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
-import { Space } from "antd";
+import { Divider, Space } from "antd";
 
 const Widget = () => {
   return (
@@ -8,8 +8,13 @@ const Widget = () => {
       <Link to="/anonymous/users/sign-in">
         <FormattedMessage id="nut.users.sign-in.title" />
       </Link>
+      <Divider type="vertical" />
       <Link to="/anonymous/users/sign-up">
         <FormattedMessage id="nut.users.sign-up.title" />
+      </Link>
+      <Divider type="vertical" />
+      <Link to="/anonymous/users/forgot-password">
+        <FormattedMessage id="nut.users.forgot-password.title" />
       </Link>
     </Space>
   );
