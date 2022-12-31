57e995986bbedcc2395fb513a44652b41aae094e
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2022-12-31 08:22:18 +0630

    :globe_with_meridians: add email.or.username

--- dashboard/src/locales/zh-Hans/forms.ts
+++ dashboard/src/locales/zh-Hans/forms.ts
@@ -1,5 +1,6 @@
 const items = {
   "forms.fields.email.label": "电子邮箱",
+  "forms.fields.email.or.username.label": "电子邮箱/用户名",
   "forms.fields.password.label": "密码",
   "forms.fields.id.label": "ID",
   "forms.fields.message.label": "消息",
