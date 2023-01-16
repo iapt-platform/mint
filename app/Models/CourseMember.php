74231cfd0993024d09468dbd8c04dea26b59d624
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-16 07:28:35 +0630

    id -> string

--- app/Models/CourseMember.php
+++ app/Models/CourseMember.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CourseMember extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id';
+    protected $casts = [
+        'id' => 'string'
+    ];
+    public  $incrementing = false;
 }
