2279b21f04c4025ddeb06f403ad552a4e721c1e3
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-16 07:29:54 +0630

    add index, store, delete

--- tests/Feature/CourseTest.php
+++ tests/Feature/CourseTest.php
@@ -13,10 +13,43 @@ class CourseTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    private $token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYmYiOjE2NjgyMzE3MTksImV4cCI6MTY5OTc2NzcxOSwidWlkIjoiYmE1NDYzZjMtNzJkMS00NDEwLTg1OGUtZWFkZDEwODg0NzEzIiwiaWQiOiI0In0.LV4ItC5VCqXpbKIXT1zePcnfi-heCf3Df63w7qbXsT1i5KJtwJJC938CLgANjqwcQFa3lrR5TqvT1kkqD-Mmgg';
+    public function test_index(){
+        $response = $this->withHeaders([
+            'Authorization' => $this->token,
+        ])->get('/api/v2/course?view=create');
 
-        $response->assertStatus(200);
+        $response->assertOk();
+
+        $response = $this->withHeaders([
+            'Authorization' => $this->token,
+        ])->get('/api/v2/course?view=study');
+
+        $response->assertOk();
+    }
+    public function test_store()
+    {
+        //testing store
+        $response = $this->withHeaders([
+            'Authorization' => $this->token,
+        ])->json('POST', '/api/v2/course',
+                    [
+                        'title'=>'course1',
+                        'studio'=>'visuddhinanda'
+                    ]);
+
+        $response->assertOk();
+    }
+
+    public function test_delete(){
+
+        //testing delete
+        $member = Course::where('title','course1')
+                        ->first();
+        $response = $this->withHeaders([
+            'Authorization' => $this->token,
+        ])->delete('/api/v2/course/'.$member->id);
+
+        $response->assertOk();
     }
 }
