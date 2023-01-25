83c5ffb464a6697cdb55e404024320b946bde3c8
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-25 21:01:18 +0630

    与服务器api连接

--- dashboard/src/components/library/course/CourseIntro.tsx
+++ dashboard/src/components/library/course/CourseIntro.tsx
@@ -1,51 +1,24 @@
 //课程详情简介
-import { Link } from "react-router-dom";
-import React from 'react';
-import { ProForm, ProFormText } from "@ant-design/pro-components";
-import {Layout,  Descriptions, Space , Col, Row } from 'antd';
+import { Col, Row } from "antd";
+import { marked } from "marked";
 
-import ReactPlayer from 'react-player'
-
-const Widget = () => {
-  
-
+interface IWidget {
+  intro?: string;
+}
+const Widget = ({ intro }: IWidget) => {
   return (
-    <ProForm.Group>
-        <Layout>
-        <Descriptions title="课程简介">
-    <Descriptions.Item label=" ">每一尊佛体证后，都会有一次殊胜的大聚会，那是十方天人的相聚，由此宣说《大集会经》。
-佛陀观察到：天人们内心有种种问题，他们却不知该如何表达……
-于是便有了化身佛在问，本尊佛在答……
- 
-根据众生的根性，佛陀共开示六部经，分别针对贪、瞋、痴、信、觉、寻六种性格习性的天人。
-此部《纷争分歧经》便是专门为瞋行者量身而作，瞋行者往往多思、多慧，佛陀便以智慧循循善诱，抽丝剥茧，层层深入，探究纷争、分歧等八种烦恼根源何在……
- 
-听，佛陀在说——
-让我们以佛陀当年的语言——古老的巴利语——去聆听佛陀的教诲……</Descriptions.Item>
-  </Descriptions>
-  <Descriptions title="电子平台课堂笔记">
-    <Descriptions.Item label="快速预览（课前预习）">
-      <Link to="/course/lesson/12345">原文</Link>  <Link to="/course/lesson/23456">原文+义注</Link> </Descriptions.Item>
-  </Descriptions>
-  <ReactPlayer
-            className='react-player fixed-bottom'
-            url= 'https://assets-hk.wikipali.org/video/admissions1080p.mp4'
-            width='50%'
-            height='50%'
-            controls = {true}
-
-          />
-    </Layout>
-      </ProForm.Group>
-    );
+    <Row>
+      <Col flex="auto"></Col>
+      <Col flex="1260px">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(intro ? intro : ""),
+          }}
+        />
+      </Col>
+      <Col flex="auto"></Col>
+    </Row>
+  );
 };
 
 export default Widget;
-
-
-/*
-<Button type="primary">关注</Button>
-<Button type="primary" disabled>
-  已关注
-</Button>
-*/
--- dashboard/src/components/library/course/CourseShow.tsx
+++ dashboard/src/components/library/course/CourseShow.tsx
@@ -1,41 +1,50 @@
 //课程详情图片标题按钮主讲人组合
 import { Link } from "react-router-dom";
-import React from 'react';
-import { ProForm, ProFormText } from "@ant-design/pro-components";
-import {Layout,  Image, Button, Space , Col, Row } from 'antd';
+import { Image, Button, Space, Col, Row } from "antd";
+import { Typography } from "antd";
 
-const Widget = () => {
-  
+import { IUser } from "../../auth/User";
+import { API_HOST } from "../../../request";
+import UserName from "../../auth/UserName";
 
+const { Title } = Typography;
+
+interface IWidget {
+  title?: string;
+  subtitle?: string;
+  coverUrl?: string;
+  teacher?: IUser;
+}
+const Widget = ({ title, subtitle, coverUrl, teacher }: IWidget) => {
   return (
-    <ProForm.Group>
-        <Layout>
-          <Row>
-    <Image
-    width={200}
-    src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg95.699pic.com%2Fxsj%2F0g%2Fk2%2F7d.jpg%21%2Ffw%2F700%2Fwatermark%2Furl%2FL3hzai93YXRlcl9kZXRhaWwyLnBuZw%2Falign%2Fsoutheast&refer=http%3A%2F%2Fimg95.699pic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1673839902&t=d8f4306ddd6935313c66efb936cbe268"
-  />
-    <h1 style={{ "fontWeight": 'bold', "fontSize": 30}}>wikipali课程</h1>
-
-          <Col flex="auto"></Col>
-          <Col flex="1260px">   </Col>
-          </Row>
-          <Col>
+    <>
+      <Row>
+        <Col flex="auto"></Col>
+        <Col flex="1260px">
+          <Space>
+            <Image
+              width={200}
+              style={{ borderRadius: 12 }}
+              src={API_HOST + "/" + coverUrl}
+              fallback={`${API_HOST}/app/course/img/default.jpg`}
+            />
+            <Space direction="vertical">
+              <Title level={3}>{title}</Title>
+              <Title level={5}>{subtitle}</Title>
+            </Space>
+          </Space>
           <Button type="primary">关注</Button>
-          </Col>
-
-
-
-
-    <p style={{ "fontWeight": 'bold', "fontSize": 15}}>主讲人： <Link to="/course/lesson/12345">小僧善巧</Link> </p>
-    
-    </Layout>
-      </ProForm.Group>
-    );
+          <div>
+            主讲人： <UserName {...teacher} />
+          </div>
+        </Col>
+        <Col flex="auto"></Col>
+      </Row>
+    </>
+  );
 };
 
 export default Widget;
-
 
 /*
 <Button type="primary">关注</Button>
