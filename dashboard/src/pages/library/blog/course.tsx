ccb9fc30d55def465407b0a4d81522264911c323
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-25 21:02:23 +0630

    :art: formatting

--- dashboard/src/pages/library/blog/course.tsx
+++ dashboard/src/pages/library/blog/course.tsx
@@ -3,14 +3,14 @@ import { useParams } from "react-router-dom";
 import BlogNav from "../../../components/blog/BlogNav";
 
 const Widget = () => {
-	// TODO
-	const { studio } = useParams(); //url 参数
+  // TODO
+  const { studio } = useParams(); //url 参数
 
-	return (
-		<>
-			<BlogNav selectedKey="course" studio={studio ? studio : ""} />
-		</>
-	);
+  return (
+    <>
+      <BlogNav selectedKey="course" studio={studio} />
+    </>
+  );
 };
 
 export default Widget;
