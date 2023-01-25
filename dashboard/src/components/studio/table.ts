b922de8131436db28de46c9c4c541fe9eae8ad1c
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-25 21:01:37 +0630

    公开的改为蓝色图标

--- dashboard/src/components/studio/table.ts
+++ dashboard/src/components/studio/table.ts
@@ -1,74 +1,74 @@
 import { useIntl } from "react-intl";
 
 export const PublicityValueEnum = () => {
-	const intl = useIntl();
-	return {
-		all: {
-			text: intl.formatMessage({
-				id: "tables.publicity.all",
-			}),
-			status: "Default",
-		},
-		0: {
-			text: intl.formatMessage({
-				id: "tables.publicity.disable",
-			}),
-			status: "Default",
-		},
-		10: {
-			text: intl.formatMessage({
-				id: "tables.publicity.private",
-			}),
-			status: "Processing",
-		},
-		20: {
-			text: intl.formatMessage({
-				id: "tables.publicity.public.bylink",
-			}),
-			status: "Processing",
-		},
-		30: {
-			text: intl.formatMessage({
-				id: "tables.publicity.public",
-			}),
-			status: "Success",
-		},
-		40: {
-			text: intl.formatMessage({
-				id: "tables.publicity.public.edit",
-			}),
-			status: "Success",
-		},
-	};
+  const intl = useIntl();
+  return {
+    all: {
+      text: intl.formatMessage({
+        id: "tables.publicity.all",
+      }),
+      status: "Default",
+    },
+    0: {
+      text: intl.formatMessage({
+        id: "tables.publicity.disable",
+      }),
+      status: "Default",
+    },
+    10: {
+      text: intl.formatMessage({
+        id: "tables.publicity.private",
+      }),
+      status: "Success",
+    },
+    20: {
+      text: intl.formatMessage({
+        id: "tables.publicity.public.bylink",
+      }),
+      status: "Processing",
+    },
+    30: {
+      text: intl.formatMessage({
+        id: "tables.publicity.public",
+      }),
+      status: "Processing",
+    },
+    40: {
+      text: intl.formatMessage({
+        id: "tables.publicity.public.edit",
+      }),
+      status: "Processing",
+    },
+  };
 };
 
 export const RoleValueEnum = () => {
-	const intl = useIntl();
-	return {
-		all: {
-			text: intl.formatMessage({
-				id: "tables.role.all",
-			}),
-		},
-		owner: {
-			text: intl.formatMessage({
-				id: "tables.role.owner",
-			}),
-		},
-		manager: {
-			text: intl.formatMessage({
-				id: "tables.role.manager",
-			}),
-		},
-		editor: {
-			text: intl.formatMessage({
-				id: "tables.role.editor",
-			}),
-		},
-		member: {
-			text: intl.formatMessage({
-				id: "tables.role.member",
-			}),
-		},
-	};
+  const intl = useIntl();
+  return {
+    all: {
+      text: intl.formatMessage({
+        id: "tables.role.all",
+      }),
+    },
+    owner: {
+      text: intl.formatMessage({
+        id: "tables.role.owner",
+      }),
+    },
+    manager: {
+      text: intl.formatMessage({
+        id: "tables.role.manager",
+      }),
+    },
+    editor: {
+      text: intl.formatMessage({
+        id: "tables.role.editor",
+      }),
+    },
+    member: {
+      text: intl.formatMessage({
+        id: "tables.role.member",
+      }),
+    },
+  };
 };
