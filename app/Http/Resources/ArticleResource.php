f56fc1c50d358756c1f1174c2a9082f97b72fdea
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-01-30 15:40:35 +0630

    由课程模块进入，渲染特别的html

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -4,6 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Api\MdRender;
+use App\Http\Api\UserApi;
+use App\Models\CourseMember;
+use App\Models\Course;
+use Illuminate\Support\Facades\Log;
 
 class ArticleResource extends JsonResource
 {
@@ -28,9 +32,48 @@ class ArticleResource extends JsonResource
             "updated_at" => $this->updated_at,
         ];
         if(isset($this->content) && !empty($this->content)){
+            if($request->has('channel')){
+                $channel = $request->get('channel');
+            }else{
+                $channel = '';
+            }
             $data["content"] = $this->content;
             $data["content_type"] = $this->content_type;
-            $data["html"] = MdRender::render($this->content,$request->get('channel'));
+            $query_id = null;
+            if($request->has('course')){
+                if($request->has('exercise')){
+                    $query_id = $request->get('exercise');
+                    if($request->has('user')){
+                        /**
+                         * 显示指定用户作业
+                         * 查询用户在课程中的channel
+                         */
+                        $userId = UserApi::getIdByName($request->get('user'));
+                        Log::info("userId:{$userId}");
+
+                        $userInCourse = CourseMember::where('course_id',$request->get('course'))
+                                    ->where('user_id',$userId)
+                                    ->first();
+                        if($userInCourse){
+                            $channel = $userInCourse->channel_id;
+                        }
+                    }else if($request->get('view')==="answer"){
+                        /**
+                         * 显示答案
+                         * 算法：查询course 答案 channel
+                         */
+                        $channel = Course::where('id',$request->get('course'))->value('channel_id');
+                    }else{
+                        //显示答案
+                        $channel = Course::where('id',$request->get('course'))->value('channel_id');
+                    }
+                }else{
+                    $channel = Course::where('id',$request->get('course'))->value('channel_id');
+                }
+            }
+            Log::info("channel:{$channel}");
+            Log::info("query_id:{$query_id}");
+            $data["html"] = MdRender::render($this->content,$channel,$query_id);
         }
         return $data;
     }
