b2cc6c3fdc9d2752912ce8d66a3d5bec4c6f68b1
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-02-06 20:30:31 +0630

    :fire: 无用的依赖

--- dashboard/src/components/course/TextBook.tsx
+++ dashboard/src/components/course/TextBook.tsx
@@ -1,5 +1,5 @@
 import { Col, Row } from "antd";
-import { NavigateOptions, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AnthologyDetail from "../article/AnthologyDetail";
 
 interface IWidget {
