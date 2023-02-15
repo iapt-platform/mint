c5cceb9631078ea7ecdd0731816be4206ac8747b
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-02-15 18:35:30 +0630

    add link of github

--- dashboard/src/components/library/FooterBar.tsx
+++ dashboard/src/components/library/FooterBar.tsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 import { Layout, Row, Col } from "antd";
-import { useIntl } from "react-intl";
 import CreateFeedback from "../feedback/CreateFeedback";
 
 const { Footer } = Layout;
 
 const Widget = () => {
   //Library foot bar
-  const intl = useIntl(); //i18n
   // TODO
   return (
     <Footer>
       <Row>
-        <Col span={8}>相关链接</Col>
+        <Col span={8}>
+          <h3>相关链接</h3>
+          <ul>
+            <li>
+              <Link to="www.github.com/iapt-platform/mint" target="_blank">
+                wikipali in github
+              </Link>
+            </li>
+            <li>nissaya project</li>
+          </ul>
+        </Col>
         <Col span={16}>
-          <div>问题反馈</div>
+          <h3>问题反馈</h3>
           <div>
             <CreateFeedback />
           </div>
