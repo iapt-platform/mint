668ac5269bf50a46437473214f11d0a6aec2027f
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-02-21 08:58:11 +0630

    使用外部导入菜单

--- dashboard/src/components/article/MainMenu.tsx
+++ dashboard/src/components/article/MainMenu.tsx
@@ -1,29 +1,11 @@
-import { Button, Dropdown, MenuProps } from "antd";
+import { Button, Dropdown } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
+import { mainMenuItems } from "../library/HeadBar";
 
 const Widget = () => {
-  const items: MenuProps["items"] = [
-    {
-      key: "new",
-      label: "最新",
-    },
-    {
-      key: "palicanon",
-      label: "圣典",
-    },
-  ];
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-    switch (e.key) {
-      case "showCol":
-        break;
-      default:
-        break;
-    }
-  };
   return (
     <Dropdown
-      menu={{ items, onClick }}
+      menu={{ items: mainMenuItems }}
       placement="bottomLeft"
       trigger={["click"]}
     >
