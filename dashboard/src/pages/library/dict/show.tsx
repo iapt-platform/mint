3184e6fdee5d56db926ec7579bb3193484891099
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-02-25 09:32:18 +0630

    add navigate

--- dashboard/src/pages/library/dict/show.tsx
+++ dashboard/src/pages/library/dict/show.tsx
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import Dictionary from "../../../components/dict/Dictionary";
 
 const Widget = () => {
   const { word } = useParams(); //url 参数
-  return <Dictionary word={word} />;
+  const navigate = useNavigate();
+  return (
+    <Dictionary
+      word={word}
+      onSearch={(value: string, isFactor?: boolean) => {
+        if (!isFactor) {
+          navigate("/dict/" + value);
+        }
+      }}
+    />
+  );
 };
 
 export default Widget;
