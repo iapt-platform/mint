5abb66898a0fb00fc8959d10adbde8ddf389b5a1
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-02-27 08:40:10 +0630

    create

---
+++ app/Console/Commands/UpgradeAt20230227.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpgradeAt20230227 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'upgrade:at20230227';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('init:system.channel');
+        $this->call('init:system.dict');
+        $this->call('upgrade:dict');
+        $this->call('upgrade:dict.vocabulary');
+        $this->call('upgrade:dict.default.meaning');
+
+        return 0;
+    }
+}
