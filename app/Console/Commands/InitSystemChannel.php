c2fbf76a3a9ed21c1d58252dafd58e947ef0c050
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-03-22 19:20:21 +0630

    add _System_Grammar_Term

--- app/Console/Commands/InitSystemChannel.php
+++ app/Console/Commands/InitSystemChannel.php
@@ -32,6 +32,21 @@ class InitSystemChannel extends Command
             'type'=>'original',
             'lang'=>'pali',
         ],
+        [
+            "name"=>'_System_Grammar_Term_zh-hans_',
+            'type'=>'translation',
+            'lang'=>'zh-Hans',
+        ],
+        [
+            "name"=>'_System_Grammar_Term_zh-hant_',
+            'type'=>'translation',
+            'lang'=>'zh-Hant',
+        ],
+        [
+            "name"=>'_System_Grammar_Term_en_',
+            'type'=>'translation',
+            'lang'=>'en',
+        ],
     ];
 
     /**
