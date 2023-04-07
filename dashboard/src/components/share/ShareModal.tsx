eeb48f4a5dac004d5033f19bbad7ab438f528a2e
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-04-07 09:08:07 +0630

    :construction: create

---
+++ dashboard/src/components/share/ShareModal.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Modal } from "antd";
+import Share, { EResType } from "./Share";
+
+interface IWidget {
+  resId: string;
+  resType: EResType;
+  trigger?: React.ReactNode;
+}
+const Widget = ({ resId, resType, trigger }: IWidget) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <span onClick={showModal}>{trigger}</span>
+      <Modal
+        destroyOnClose={true}
+        width={700}
+        title="协作"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Share resId={resId} resType={resType} />
+      </Modal>
+    </>
+  );
+};
+
+export default Widget;
