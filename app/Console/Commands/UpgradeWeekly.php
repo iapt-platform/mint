06b7075007b32287eb31f326afd301d449bf1cc4
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-04-10 19:27:58 +0630

    刷进度图改为每周任务

---
+++ app/Console/Commands/UpgradeWeekly.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpgradeWeekly extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'upgrade:weekly';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '周更';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        # 段落更新图
+        $this->call('upgrade:chapterdynamic');
+        $this->call('upgrade:chapter.dynamic.weekly');
+        $this->call('export:offline');
+
+        return 0;
+    }
+}
