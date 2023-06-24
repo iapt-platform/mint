8620e4cad4eac49c6110ff2a6e74b0bc1982fa34
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-06-24 18:56:30 +0630

    echo Str::markdown($markdown);

--- app/Console/Commands/TestMdRender.php
+++ app/Console/Commands/TestMdRender.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Http\Api\MdRender;
+use Illuminate\Support\Str;
 
 class TestMdRender extends Command
 {
@@ -45,6 +46,8 @@ class TestMdRender extends Command
         //$markdown .= "多**行注**释\n\n";
         //$markdown .= "多行注释\n";
         //$markdown .= "}}\n\n";
+        $markdown .= "## heading \n\n";
+        $markdown .= "ddd \n\n";
         $markdown .= "- title \n";
         $markdown .= "  \n";
         $markdown .= "  content-1\n";
@@ -79,7 +82,7 @@ class TestMdRender extends Command
         //$html = MdRender::xmlQueryId($xml, "1");
         //$sent = MdRender::take_sentence($html);
         //print_r($sent);
-
+        echo Str::markdown($markdown);
         echo MdRender::render2($markdown,'00ae2c48-c204-4082-ae79-79ba2740d506',null,'read','nissaya');
         return 0;
     }
