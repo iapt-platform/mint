c89e9d2581f28c09539f47c05b29edf628b2c1e5
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-07-13 15:05:25 +0630

    create

--- resources/views/emails/invite_en.blade copy.php
+++ resources/views/emails/invite_en.blade copy.php
@@ -6,7 +6,7 @@
 <body>
 wikipali 注册邀请
 点击此链接注册wikipali账号。此链接邀请只可以注册一个账户。有效期7天。
-<a href='{{ $url }}{{ $uuid }}'>{{ $url }}{{ $uuid }}</a><br>
+<a href='{{ $url }}'>{{ $url }}</a><br>
 此邮件为系统自动发送，请勿回复。
 </body>
 </html>
