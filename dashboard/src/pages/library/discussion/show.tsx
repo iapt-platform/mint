0e158d3cc86008b90fd414884bc52260ba3a333c
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-07-15 22:23:39 +0630

    add DiscussionAnchor

--- dashboard/src/pages/library/discussion/show.tsx
+++ dashboard/src/pages/library/discussion/show.tsx
@@ -1,18 +1,30 @@
-import { useParams } from "react-router-dom";
-
-import CommentTopic from "../../../components/discussion/DiscussionTopic";
+import { Divider } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import DiscussionAnchor from "../../../components/discussion/DiscussionAnchor";
+import { IComment } from "../../../components/discussion/DiscussionItem";
+import DiscussionListCard, {
+  TResType,
+} from "../../../components/discussion/DiscussionListCard";
 
 const Widget = () => {
   // TODO
-  const { id } = useParams(); //url 参数
+  const { type, id } = useParams(); //url 参数
+  const navigate = useNavigate();
 
   return (
-    <div>
-      <div>锚点</div>
-      <div>
-        <CommentTopic topicId={id} />
-      </div>
-    </div>
+    <>
+      <DiscussionAnchor resId={id} resType={type as TResType} />
+      <Divider></Divider>
+      <DiscussionListCard
+        resId={id}
+        onSelect={(
+          e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+          comment: IComment
+        ) => {
+          navigate(`/discussion/topic/${comment.id}`);
+        }}
+      />
+    </>
   );
 };
 
