2f319f30227657a2da2087d801f65b4150501dc6
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-07-16 09:39:34 +0630

    protected $primaryKey = 'uid';

--- app/Models/Sentence.php
+++ app/Models/Sentence.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Sentence extends Model
 {
     use HasFactory;
-	protected $fillable = ['id','uid','book_id','paragraph','word_start','word_end','channel_uid','editor_uid','content','strlen','status','create_time','modify_time','language'];
+	protected $fillable = ['id','uid','book_id',
+                          'paragraph','word_start','word_end',
+                          'channel_uid','editor_uid','content',
+                          'strlen','status','create_time','modify_time','language'];
+    protected $primaryKey = 'uid';
+    protected $casts = [
+        'uid' => 'string'
+    ];
+
 }
