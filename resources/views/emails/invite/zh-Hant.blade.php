828a1496b711a5410d986953f31d56aeae06a3d7
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-07-21 12:02:06 +0630

    create

---
+++ resources/views/emails/invite/zh-Hant.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>invite</title>
+    </head>
+<body>
+<div>wikipali 註冊邀請</div>
+<div>點選此鏈接註冊wikipali賬號。此鏈接邀請隻可以註冊一個賬戶。</div>
+<div><a href='{{ $url }}'>{{ $url }}</a></div>
+<div>此郵件為係統自動發送，請勿回複。</div>
+</body>
+</html>
