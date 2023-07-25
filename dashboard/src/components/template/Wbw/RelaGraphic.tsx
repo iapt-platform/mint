fc50bff55ded6f4a4b06207900b6fb8c65bfb41a
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-07-25 12:15:01 +0630

    IRelation rename IWbwRelation

--- dashboard/src/components/template/Wbw/RelaGraphic.tsx
+++ dashboard/src/components/template/Wbw/RelaGraphic.tsx
@@ -1,7 +1,7 @@
 import Mermaid from "../../general/Mermaid";
 import { useAppSelector } from "../../../hooks";
 import { getTerm } from "../../../reducers/term-vocabulary";
-import { IRelation } from "./WbwDetailRelation";
+import { IWbwRelation } from "./WbwDetailRelation";
 import { IWbw } from "./WbwWord";
 
 interface IWidget {
@@ -18,7 +18,7 @@ const RelaGraphicWidget = ({ wbwData }: IWidget) => {
       ?.filter((value) => value.relation)
       .map((item) => {
         if (item.relation && item.relation.value) {
-          const json: IRelation[] = JSON.parse(item.relation.value);
+          const json: IWbwRelation[] = JSON.parse(item.relation.value);
           const graphic = json.map((relation) => {
             const localName = terms?.find(
               (item) => item.word === relation.relation
