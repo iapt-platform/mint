ae93d534a367f2a3b1e8abaccba0d1f1bbdab6c7
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-08-02 09:15:03 +0630

    add onArticleSelect

--- dashboard/src/pages/library/anthology/show.tsx
+++ dashboard/src/pages/library/anthology/show.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Col, Row } from "antd";
 
 import AnthologyDetail from "../../../components/article/AnthologyDetail";
@@ -6,6 +6,7 @@ import AnthologyDetail from "../../../components/article/AnthologyDetail";
 const Widget = () => {
   // TODO
   const { id } = useParams(); //url 参数
+  const navigate = useNavigate();
 
   const pageMaxWidth = "960px";
   return (
@@ -13,7 +14,16 @@ const Widget = () => {
       <Row>
         <Col flex="auto"></Col>
         <Col flex={pageMaxWidth}>
-          <AnthologyDetail aid={id} />
+          <AnthologyDetail
+            onArticleSelect={(anthologyId: string, keys: string[]) => {
+              if (keys[0]) {
+                navigate(
+                  `/article/article/${keys[0]}?mode=read&anthology=${anthologyId}`
+                );
+              }
+            }}
+            aid={id}
+          />
         </Col>
         <Col flex="auto"></Col>
       </Row>
