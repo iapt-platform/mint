aa90f1ea0bd958e1713328df82c44cd29a0f3f6b
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-08-10 09:51:28 +0630

    支持侧边打开

--- dashboard/src/components/template/SentEdit/SuggestionBox.tsx
+++ dashboard/src/components/template/SentEdit/SuggestionBox.tsx
@@ -45,14 +45,62 @@ const SuggestionBoxWidget = ({
       onClose();
     }
   };
-
+  const suggestion = (
+    <Space direction="vertical" style={{ width: "100%" }}>
+      <Card
+        title="温馨提示"
+        size="small"
+        style={{
+          width: "100%",
+          display: openNotification ? "block" : "none",
+        }}
+      >
+        <Marked
+          text="此处专为提交修改建议译文。您输入的应该是**译文**
+    而不是评论和问题。其他内容，请在讨论页面提交。"
+        />
+        <p style={{ textAlign: "center" }}>
+          <Button
+            onClick={() => {
+              localStorage.setItem("read_pr_Notification", "ok");
+              setOpenNotification(false);
+            }}
+          >
+            知道了
+          </Button>
+        </p>
+      </Card>
+      <SuggestionAdd
+        data={data}
+        onCreate={() => {
+          setReload(true);
+        }}
+      />
+      <SuggestionList
+        {...data}
+        reload={reload}
+        onReload={() => setReload(false)}
+        onChange={(count: number) => setPrNumber(count)}
+      />
+    </Space>
+  );
   return (
     <>
       <Space onClick={showDrawer}>
         {trigger}
         {prNumber}
       </Space>
-
+      <div
+        style={{
+          position: "absolute",
+          display: isOpen ? "none" : "none",
+          zIndex: 1030,
+          marginLeft: 300,
+          marginTop: -250,
+        }}
+      >
+        {suggestion}
+      </div>
       <Drawer
         title="修改建议"
         width={520}
@@ -60,43 +108,7 @@ const SuggestionBoxWidget = ({
         open={isOpen}
         maskClosable={false}
       >
-        <Space direction="vertical" style={{ width: "100%" }}>
-          <Card
-            title="温馨提示"
-            size="small"
-            style={{
-              width: "100%",
-              display: openNotification ? "block" : "none",
-            }}
-          >
-            <Marked
-              text="此处专为提交修改建议译文。您输入的应该是**译文**
-              而不是评论和问题。其他内容，请在讨论页面提交。"
-            />
-            <p style={{ textAlign: "center" }}>
-              <Button
-                onClick={() => {
-                  localStorage.setItem("read_pr_Notification", "ok");
-                  setOpenNotification(false);
-                }}
-              >
-                知道了
-              </Button>
-            </p>
-          </Card>
-          <SuggestionAdd
-            data={data}
-            onCreate={() => {
-              setReload(true);
-            }}
-          />
-          <SuggestionList
-            {...data}
-            reload={reload}
-            onReload={() => setReload(false)}
-            onChange={(count: number) => setPrNumber(count)}
-          />
-        </Space>
+        {suggestion}
       </Drawer>
     </>
   );
