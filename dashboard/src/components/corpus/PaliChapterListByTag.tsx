89a5ec80551db7f18b63af321c3990e9b6895e25
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-08-16 15:35:30 +0630

    :fire: log

--- dashboard/src/components/corpus/PaliChapterListByTag.tsx
+++ dashboard/src/components/corpus/PaliChapterListByTag.tsx
@@ -14,13 +14,12 @@ const PaliChapterListByTagWidget = ({ tag = [], onChapterClick }: IWidget) => {
   const [tableData, setTableData] = useState<IPaliChapterData[]>([]);
 
   useEffect(() => {
-    console.log("palichapterlist useEffect");
     if (tag.length === 0) {
       setTableData([]);
       return;
     }
     let url = `/v2/palitext?view=chapter&tags=${tag.join()}`;
-    console.log("tag url", url);
+    console.log("url", url);
     get<IPaliChapterListResponse>(url).then((json) => {
       if (json.ok) {
         let newTree: IPaliChapterData[] = json.data.rows.map((item) => {
