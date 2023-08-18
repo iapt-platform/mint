f5bc73adbf35373a4561c821e4ad0388b2462784
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-08-18 09:59:31 +0630

    add use cache

--- app/Console/Commands/ExportOffline.php
+++ app/Console/Commands/ExportOffline.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 class ExportOffline extends Command
 {
