5f20401536b8fe767dfa289be5c4f5bd5498bb1b
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-09-08 17:19:03 +0630

    add morus render

--- app/Tools/Markdown.php
+++ app/Tools/Markdown.php
@@ -5,26 +5,24 @@ use Illuminate\Support\Str;
 class Markdown
 {
     public static function render($text){
-        return Markdown::strdown($text);
+        return Markdown::morus($text);
     }
-    public static function marked($text){
-            $host = env('MORUS_RPC_SERVER');
-            $client = new \Mint\Morus\V1\MarkdownClient($host, [
-                'credentials' => \Grpc\ChannelCredentials::createInsecure(),
-            ]);
-            $request = new \Mint\Morus\V1\MarkdownToHtmlRequest();
-            $request->setPayload($text);
-            $request->setSanitize(true);
-            list($response, $status) = $client->ToHtml($request)->wait();
-            if ($status->code !== \Grpc\STATUS_OK) {
-                return "ERROR: " . $status->code . ", " . $status->details;
-            }
-            return $response->getPayload();
+
+    public static function morus($text){
+        $host = env('MORUS_RPC_SERVER');
+        $client = new \Mint\Morus\V1\MarkdownClient($host, [
+            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        ]);
+        $request = new \Mint\Morus\V1\MarkdownToHtmlRequest();
+        $request->setPayload($text);
+        $request->setSanitize(true);
+        list($response, $status) = $client->ToHtml($request)->wait();
+        if ($status->code !== \Grpc\STATUS_OK) {
+            return "ERROR: " . $status->code . ", " . $status->details;
+        }
+        return $response->getPayload();
     }
-    public static function parsedown($text){
-        $Parsedown = new \Parsedown();
-        return $Parsedown->text($text);
-    }
+
     public static function strdown($text){
         return Str::markdown($text);
     }
