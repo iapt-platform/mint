c8b93c1a0b77ecd7945b4f619c136fc1d0596a24
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-09-28 19:57:40 +0630

    :fire: app/ucenter/function.php

--- app/Http/Api/UserApi.php
+++ app/Http/Api/UserApi.php
@@ -3,22 +3,17 @@ namespace App\Http\Api;
 use App\Models\UserInfo;
 use Illuminate\Support\Facades\Log;
 
-require_once __DIR__.'/../../../public/app/ucenter/function.php';
-
 class UserApi{
     public static function getIdByName($name){
-        $userinfo = new \UserInfo();
-        return $userinfo->getUserByName($name)['userid'];
+        return UserInfo::where('username',$name)->value('userid');
     }
     public static function getIdByUuid($uuid){
         return UserInfo::where('userid',$uuid)->value('id');
     }
     public static function getIntIdByName($name){
-        $userinfo = new \UserInfo();
-        return $userinfo->getUserByName($name)['id'];
+        return UserInfo::where('username',$name)->value('id');
     }
     public static function getById($id){
-
         $user = UserInfo::where('id',$id)->first();
         if($user){
             return [
