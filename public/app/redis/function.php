f8b0b721c2365a136ef7346db9c49ba053b8c2b7
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-10-18 21:16:55 +0630

    add RedisCluster

--- public/app/redis/function.php
+++ public/app/redis/function.php
@@ -3,17 +3,23 @@ require_once __DIR__."/../config.php";
 function redis_connect()
 {
 	try {
-		//code...
-		$redis = new redis();
-		$r_conn = $redis->connect(Redis["host"], Redis["port"]);
+        $server = $_SERVER['SERVER_NAME'];
+        if(strpos($server,'wikipali') === false){
+            //local dev
+            $redis = new redis();
+            $r_conn = $redis->connect(Redis["host"], Redis["port"]);
+        }else{
+            $redis = new RedisCluster(NULL,Array(Redis["host"].':'.Redis["port"]));
+            $r_conn = true;
+        }
 		if ($r_conn) {
 			if(Redis["password"] !== ""){
 				$redis->auth(Redis["password"]);
-			}		
+			}
 			return $redis;
 		} else {
 			return false;
-		}		
+		}
 	} catch (\Throwable $th) {
 		//throw $th;
 		return false;
