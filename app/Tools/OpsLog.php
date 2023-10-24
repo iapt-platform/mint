f5987fdd4026ffa83ceb739ea84fcb91b8b08adf
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-10-24 17:13:56 +0630

    add try

--- app/Tools/OpsLog.php
+++ app/Tools/OpsLog.php
@@ -6,12 +6,16 @@ use Illuminate\Support\Facades\Log;
 
 class OpsLog{
     public static function debug($user_uid,$content){
-        $user = UserApi::getByUuid($user_uid);
-        if($user){
-            $json = json_encode($content, JSON_UNESCAPED_UNICODE);
-            Log::debug("user({$user_uid},{$user['nickName']}, {$user['userName']},) {$json}");
-        }else{
-            Log::error('no user uuid ,',$user_uid);
+        try{
+            $user = UserApi::getByUuid($user_uid);
+            if($user){
+                $json = json_encode($content, JSON_UNESCAPED_UNICODE);
+                Log::debug("user({$user_uid},{$user['nickName']}, {$user['userName']},) {$json}");
+            }else{
+                Log::error('no user uuid ,',$user_uid);
+            }
+        }catch(\Exception $e){
+            Log::error($e);
         }
     }
 }
