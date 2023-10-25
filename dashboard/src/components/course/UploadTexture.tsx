8f515fd0f72849c5b33ce24732f2c491d3e652c8
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-10-25 08:51:36 +0630

    :fire: action

--- dashboard/src/components/course/UploadTexture.tsx
+++ dashboard/src/components/course/UploadTexture.tsx
@@ -56,7 +56,6 @@ const UploadTextureWidget = () => {
       listType="picture-card"
       className="avatar-uploader"
       showUploadList={false}
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
       beforeUpload={beforeUpload}
       onChange={handleChange}
     >
