b02d13de3f3918d7370dbd3733bc3c04e40e6183
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-10-30 17:53:03 +0630

    add message.auth.guest.alert

--- dashboard/src/locales/zh-Hans/message.ts
+++ dashboard/src/locales/zh-Hans/message.ts
@@ -2,6 +2,7 @@ const items = {
   "flashes.success": "操作成功",
   "message.irrevocable": "此操作不可撤销",
   "message.delete.confirm": "确定删除吗？",
+  "message.auth.guest.alert": "您尚未登录，部分功能无法使用。",
 };
 
 export default items;
