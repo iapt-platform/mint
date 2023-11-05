8b66d79fab849f18c781e4c449246752410c7c51
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-11-05 20:43:17 +0630

    create

---
+++ rpc/tulip/tulip/config.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+define("Config", [
+    'port' => 9990,
+    "database" => [
+        "driver" => "pgsql",
+        "host" => "localhost",
+        "port" => 5432,
+        "name" => "wikipali",
+        "user" => "postgres",
+        "password" => "123456",
+    ],
+]);
