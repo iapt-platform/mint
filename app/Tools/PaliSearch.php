e18984d3bb93aeaafafaeecdf1f7a0be6f732da1
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-11-05 21:43:51 +0630

    add $index,$size 参数

--- app/Tools/PaliSearch.php
+++ app/Tools/PaliSearch.php
@@ -5,23 +5,28 @@ use Illuminate\Support\Facades\Log;
 
 class PaliSearch
 {
-    public static function search($words,$book,$page){
+    public static function search($words,$book,$index,$size){
         $host = config('mint.server.rpc.tulip');
         Log::debug('tulip host='.$host);
         $client = new \Mint\Tulip\V1\SearchClient($host, [
             'credentials' => \Grpc\ChannelCredentials::createInsecure(),
         ]);
+
         $request = new \Mint\Tulip\V1\SearchRequest();
         $request->setKeywords($words);
         $request->setBook($book);
+        $page = new \Mint\Tulip\V1\SearchRequest\Page;
+        $page->setIndex($index);
+        $page->setSize($size);
         $request->setPage($page);
 
         list($response, $status) = $client->Pali($request)->wait();
         if ($status->code !== \Grpc\STATUS_OK) {
             Log::error("ERROR: " . $status->code . ", " . $status->details);
-            return $text;
+            return false;
         }
-        return $response->getPayload();
+        return $response->getItems();
     }
+
 
 }
