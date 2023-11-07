9be074fbed6aaccdc765bd1abd2d9d3972a7f349
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-11-07 21:39:12 +0630

    add PaliSearch::update

--- app/Console/Commands/TestSearchPali.php
+++ app/Console/Commands/TestSearchPali.php
@@ -42,7 +42,14 @@ class TestSearchPali extends Command
         //var_dump($result);
         //$result = PaliSearch::book_list(['citta'],[93,94],'case',0,10);
         //var_dump($result);
-        $this->info(PaliSearch::upload_dict('ddd'));
+        //$this->info(PaliSearch::upload_dict('ddd'));
+        $full = '19 . ‘ ‘ ‘ Yathā vā paneke bhonto samaṇabrāhmaṇā saddhādeyyāni bhojanāni bhuñjitvā te evarūpaṃ dūteyyapahiṇagamanānuyogaṃ anuyuttā viharanti , seyyathidaṃ – raññaṃ , rājamahāmattānaṃ , khattiyānaṃ , brāhmaṇānaṃ , gahapatikānaṃ , kumārānaṃ ‘ ‘ idha gaccha , amutrāgaccha , idaṃ hara , amutra idaṃ āharā ’ ’ ti iti vā iti evarūpā dūteyyapahiṇagamanānuyogā paṭivirato samaṇo gotamo ’ ti – iti vā hi , bhikkhave , puthujjano tathāgatassa vaṇṇaṃ vadamāno vadeyya . ';
+        $test = 'bhonto samaṇabrāhmaṇā';
+        $update = PaliSearch::update(93,50,
+                                        '','','',
+                                        $test,
+                                        99);
+        $this->info($update);
         return 0;
     }
 }
