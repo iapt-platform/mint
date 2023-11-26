4a5cf9f660433ef6bd6efd76a27494f9fdcb2fe6
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-11-26 22:05:38 +0630

    add curr.paragraph

--- dashboard/src/locales/zh-Hans/label.ts
+++ dashboard/src/locales/zh-Hans/label.ts
@@ -25,6 +25,10 @@ const items = {
   "labels.page.number.type.V": "印度内观中心版",
   "labels.page.number.type.O": "其他",
   "labels.page.number.type.para": "缅文段落",
+  "labels.curr.paragraph.only": "仅显示此段",
+  "labels.curr.paragraph.open": "在新标签页打开",
+  "labels.curr.paragraph.copy.tpl": "复制句子模版",
+  "labels.curr.paragraph.quote.link.tpl": "引用链接模版",
 };
 
 export default items;
