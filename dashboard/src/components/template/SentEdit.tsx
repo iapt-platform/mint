f021725d09ba297c9877e26e31194b0a1839240f
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-11-29 17:45:04 +0630

    灰色底色

--- dashboard/src/components/template/SentEdit.tsx
+++ dashboard/src/components/template/SentEdit.tsx
@@ -132,6 +132,7 @@ export const SentEditInner = ({
         border: "2px solid rgb(128 128 128 / 30%)",
         marginTop: 4,
         borderRadius: 6,
+        backgroundColor: "rgb(245 245 245)",
       }}
       size="small"
     >
