534d3efc5252c7d67375cbab6bd3355850464b57
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-02 21:34:30 +0630

    复制句子编号

--- dashboard/src/locales/zh-Hans/label.ts
+++ dashboard/src/locales/zh-Hans/label.ts
@@ -27,7 +27,7 @@ const items = {
   "labels.page.number.type.para": "缅文段落",
   "labels.curr.paragraph.only": "仅显示此段",
   "labels.curr.paragraph.open": "在新标签页打开",
-  "labels.curr.paragraph.copy.tpl": "复制句子模版",
+  "labels.curr.paragraph.copy.tpl": "复制句子编号",
   "labels.curr.paragraph.quote.link.tpl": "引用链接模版",
 };
 
