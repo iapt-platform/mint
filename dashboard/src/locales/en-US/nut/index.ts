23d6d0f2725e0ede3858298bccbb1739d011ebca
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-15 22:20:25 +0630

    create

--- dashboard/src/locales/en-US/nut/index.ts
+++ dashboard/src/locales/en-US/nut/index.ts
@@ -1,3 +1,8 @@
-const items = {};
+const items = {
+  "nut.users.sign-in.title": "登录",
+  "nut.users.sign-up.title": "新用户注册",
+  "nut.users.logs.title": "日志列表",
+  "nut.users.forgot-password.title": "忘记密码",
+};
 
 export default items;
