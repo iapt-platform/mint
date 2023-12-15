2568aafb2eab14a19c84095e58f76024009c556d
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-15 22:17:15 +0630

    create

---
+++ dashboard/src/locales/en-US/article/index.ts
@@ -0,0 +1,6 @@
+const items = {
+	"article.lable": "文章",
+	"article.fields.article.count.label": "文章数量",
+};
+
+export default items;
---
+++ dashboard/src/locales/en-US/message.ts
@@ -0,0 +1,10 @@
+const items = {
+  "flashes.success": "操作成功",
+  "message.irrevocable": "此操作不可撤销",
+  "message.delete.confirm": "确定删除吗？",
+  "message.auth.guest.alert":
+    "您尚未登录，只能查看公开数据，其他功能无法使用。",
+  "message.channel.empty.alert": "没有版本风格被选择,仅显示原文。",
+};
+
+export default items;
