0643b0ec3f6edf27171655b9ac39ca3d583d6d0c
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-18 18:47:20 +0630

    add <HeadBar />

--- dashboard/src/pages/library/notifications/index.tsx
+++ dashboard/src/pages/library/notifications/index.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
 
 import FooterBar from "../../../components/library/FooterBar";
+import HeadBar from "../../../components/library/HeadBar";
 
 const Widget = () => {
   return (
     <Layout>
+      <HeadBar />
       <Outlet />
       <FooterBar />
     </Layout>
