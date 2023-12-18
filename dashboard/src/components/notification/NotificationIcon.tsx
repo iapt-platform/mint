6b7dbf59304ebb7ffc2d340a1ed56c238e77ac9c
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-18 18:38:24 +0630

    create

---
+++ dashboard/src/components/notification/NotificationIcon.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { NotificationIcon } from "../../assets/icon";
+import { Badge, Popover } from "antd";
+import { get } from "../../request";
+import { INotificationListResponse } from "../api/notification";
+import NotificationList from "./NotificationList";
+
+const NotificationIconWidget = () => {
+  const [count, setCount] = useState<number>();
+  useEffect(() => {
+    let timer = setInterval(() => {
+      const url = `/v2/notification?view=to&status=unread&limit=1`;
+      get<INotificationListResponse>(url).then((json) => {
+        if (json.ok) {
+          setCount(json.data.count);
+          if (json.data.count > 0) {
+            const newMessageTime = json.data.rows[0].created_at;
+            const lastTime = localStorage.getItem("notification/new");
+            if (lastTime === null || lastTime !== newMessageTime) {
+              localStorage.setItem("notification/new", newMessageTime);
+              if (window.Notification && Notification.permission !== "denied") {
+                Notification.requestPermission(function (status) {
+                  const notification = new Notification("通知标题", {
+                    body: json.data.rows[0].content,
+                  });
+                  notification.onclick = (event) => {
+                    event.preventDefault(); // 阻止浏览器聚焦于 Notification 的标签页
+                    window.open(json.data.rows[0].url, "_blank");
+                  };
+                });
+              }
+            }
+          }
+        }
+      });
+    }, 1000 * 60);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <>
+      <Popover
+        placement="bottomLeft"
+        arrowPointAtCenter
+        destroyTooltipOnHide
+        content={
+          <div style={{ width: 600 }}>
+            <NotificationList onChange={(unread: number) => setCount(unread)} />
+          </div>
+        }
+        trigger="click"
+      >
+        <Badge count={count} size="small">
+          <span style={{ color: "white", cursor: "pointer" }}>
+            <NotificationIcon />
+          </span>
+        </Badge>{" "}
+      </Popover>
+    </>
+  );
+};
+
+export default NotificationIconWidget;
---
+++ dashboard/src/pages/library/notifications/list.tsx
@@ -0,0 +1,8 @@
+import NotificationList from "../../../components/notification/NotificationList";
+
+const Widget = () => {
+  // TODO i18n
+  return <NotificationList />;
+};
+
+export default Widget;
