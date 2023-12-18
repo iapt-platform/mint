bfe496a7ff644896a296eae8c740bb64bf909810
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-18 18:55:25 +0630

    add getChapterPath

--- app/Http/Api/PaliTextApi.php
+++ app/Http/Api/PaliTextApi.php
@@ -15,4 +15,11 @@ class PaliTextApi{
         $end = $para + $chapter->chapter_len -1;
         return [$start,$end];
     }
+
+    public static function getChapterPath($book,$para){
+        $path = PaliText::where('book',$book)
+                        ->where('paragraph',$para)
+                        ->value('path');
+        return $path;
+    }
 }
