d5b7b9deb83d2529d26e274f239e13c0b5a72bda
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-18 18:39:58 +0630

    add style

--- dashboard/src/components/general/Marked.tsx
+++ dashboard/src/components/general/Marked.tsx
@@ -5,12 +5,14 @@ const { Text } = Typography;
 
 interface IWidget {
   text?: string;
+  style?: React.CSSProperties;
   className?: string;
 }
-const MarkedWidget = ({ text, className }: IWidget) => {
+const MarkedWidget = ({ text, style, className }: IWidget) => {
   return (
     <Text className={className}>
       <div
+        style={style}
         className={className}
         dangerouslySetInnerHTML={{
           __html: marked.parse(text ? text : ""),
