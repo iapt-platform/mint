a143d4b6a6331ba08bd13c75cedf7762ea6073e0
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2023-12-24 07:45:36 +0630

    add style

--- dashboard/src/components/auth/ToStudio.tsx
+++ dashboard/src/components/auth/ToStudio.tsx
@@ -5,27 +5,32 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../../hooks";
 import { currentUser as _currentUser } from "../../reducers/current-user";
 
-const ToStudioWidget = () => {
+interface IWidget {
+  style?: React.CSSProperties;
+}
+const ToStudioWidget = ({ style }: IWidget) => {
   const intl = useIntl();
 
   const user = useAppSelector(_currentUser);
 
   if (typeof user !== "undefined") {
     return (
-      <Button
-        type="primary"
-        style={{
-          paddingLeft: 18,
-          paddingRight: 18,
-          backgroundColor: "#52974e",
-        }}
-      >
-        <Link to={`/studio/${user.realName}/home`} target="_blank">
-          {intl.formatMessage({
-            id: "columns.studio.title",
-          })}
-        </Link>
-      </Button>
+      <span style={style}>
+        <Button
+          type="primary"
+          style={{
+            paddingLeft: 18,
+            paddingRight: 18,
+            backgroundColor: "#52974e",
+          }}
+        >
+          <Link to={`/studio/${user.realName}/home`} target="_blank">
+            {intl.formatMessage({
+              id: "columns.studio.title",
+            })}
+          </Link>
+        </Button>
+      </span>
     );
   } else {
     return <></>;
