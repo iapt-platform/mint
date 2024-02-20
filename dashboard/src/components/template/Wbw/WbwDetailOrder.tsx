63d1e0161a813b1b3f4b0b934b3b3ca0c664a2cb
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-02-20 14:44:39 +0630

    添加弹窗可见性

--- dashboard/src/components/template/Wbw/WbwDetailOrder.tsx
+++ dashboard/src/components/template/Wbw/WbwDetailOrder.tsx
@@ -7,24 +7,26 @@ import { GetUserSetting } from "../../auth/setting/default";
 interface IWidget {
   sn: number;
   curr: number;
+  visible?: boolean;
   onChange?: Function;
 }
 
-const WbwDetailOrderWidget = ({ sn, curr, onChange }: IWidget) => {
+const WbwDetailOrderWidget = ({
+  sn,
+  curr,
+  visible = false,
+  onChange,
+}: IWidget) => {
   const [show, setShow] = useState(true);
-  const [enable, setEnable] = useState(false);
   const settings = useAppSelector(settingInfo);
 
-  useEffect(() => {
-    const showOrder = GetUserSetting("setting.wbw.order", settings);
-    if (typeof showOrder === "boolean") {
-      setEnable(showOrder);
-    }
-  }, [settings]);
+  const showOrder = GetUserSetting("setting.wbw.order", settings);
+  const enable = visible && showOrder;
 
   useEffect(() => {
     setShow(sn === curr);
   }, [curr, sn]);
+
   return enable ? (
     <Tooltip
       open={show}
