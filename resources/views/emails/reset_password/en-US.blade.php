06b8609aa7d0ddc85a03c3343b11af75583bb0ea
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-03-20 09:03:02 +0630

    create

---
+++ resources/views/emails/reset_password/en-US.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>reset password</title>
+    </head>
+<body>
+<div>wikipali reset password</div>
+<div>We receive request of reset password. If you </div>
+<div>click link reset password</div>
+<div><a href='{{ $url }}'>{{ $url }}</a></div>
+<div>This email is sent automatically by system, please don't reply.</div>
+</body>
+</html>
