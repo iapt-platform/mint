ba2a5ffd0409da357d1f70e39d5a9d3ddf05ef8f
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-03-27 09:12:07 +0630

    add public_no_list

--- dashboard/src/locales/en-US/forms.ts
+++ dashboard/src/locales/en-US/forms.ts
@@ -21,6 +21,7 @@ const items = {
   "forms.fields.publicity.label": "publicity",
   "forms.fields.publicity.disable.label": "disable",
   "forms.fields.publicity.private.label": "private",
+  "forms.fields.publicity.public_no_list.label": "public no list",
   "forms.fields.publicity.public.label": "public",
   "forms.fields.teacher.label": "teacher",
   "forms.fields.studentsassistant.label": "students / assistant",
