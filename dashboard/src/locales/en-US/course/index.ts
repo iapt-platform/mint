e226c854c3685bb04cfc08300372551585dc662b
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-01 18:02:44 +0630

    apply 报名

--- dashboard/src/locales/en-US/course/index.ts
+++ dashboard/src/locales/en-US/course/index.ts
@@ -3,13 +3,14 @@ const items = {
   "course.exp.start.label": "起始经验",
   "course.exp.end.label": "结束经验",
   "course.exp.current.label": "当前经验",
+  "course.member.status.none.label": "",
   "course.member.status.normal.label": "报名成功",
   "course.member.status.joined.label": "已经参加了",
   "course.member.status.join.button": "参加",
   "course.member.status.join.message":
     "参加{course}吗？本课程为自学课程。加入后可以立即开始学习。",
-  "course.member.status.applied.label": "已经邀请",
-  "course.member.status.apply.button": "邀请",
+  "course.member.status.applied.label": "已经报名",
+  "course.member.status.apply.button": "报名",
   "course.member.status.apply.message":
     "要报名{course}吗？本课程为需审核课程。报名后请等待组织者审核录取。",
   "course.member.status.canceled.label": "已经取消",
