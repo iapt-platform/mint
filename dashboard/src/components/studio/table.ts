c52e68793ff97b801051ba4add5ecfef258cbb0c
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-03 18:27:44 +0630

    tables -> forms.fields

--- dashboard/src/components/studio/table.ts
+++ dashboard/src/components/studio/table.ts
@@ -5,43 +5,43 @@ export const PublicityValueEnum = () => {
   return {
     all: {
       text: intl.formatMessage({
-        id: "tables.publicity.all",
+        id: "forms.fields.publicity.all.label",
       }),
       status: "Default",
     },
     0: {
       text: intl.formatMessage({
-        id: "tables.publicity.disable",
+        id: "forms.fields.publicity.disable.label",
       }),
       status: "Default",
     },
     5: {
       text: intl.formatMessage({
-        id: "tables.publicity.blocked",
+        id: "forms.fields.publicity.blocked.label",
       }),
       status: "Default",
     },
     10: {
       text: intl.formatMessage({
-        id: "tables.publicity.private",
+        id: "forms.fields.publicity.private.label",
       }),
       status: "Success",
     },
     20: {
       text: intl.formatMessage({
-        id: "tables.publicity.public.bylink",
+        id: "forms.fields.publicity.public_no_list.label",
       }),
       status: "Processing",
     },
     30: {
       text: intl.formatMessage({
-        id: "tables.publicity.public",
+        id: "forms.fields.publicity.public.label",
       }),
       status: "Processing",
     },
     40: {
       text: intl.formatMessage({
-        id: "tables.publicity.public.edit",
+        id: "forms.fields.publicity.public.edit.label",
       }),
       status: "Processing",
     },
@@ -53,27 +53,27 @@ export const RoleValueEnum = () => {
   return {
     all: {
       text: intl.formatMessage({
-        id: "tables.role.all",
+        id: "auth.role.all",
       }),
     },
     owner: {
       text: intl.formatMessage({
-        id: "tables.role.owner",
+        id: "auth.role.owner",
       }),
     },
     manager: {
       text: intl.formatMessage({
-        id: "tables.role.manager",
+        id: "auth.role.manager",
       }),
     },
     editor: {
       text: intl.formatMessage({
-        id: "tables.role.editor",
+        id: "auth.role.editor",
       }),
     },
     member: {
       text: intl.formatMessage({
-        id: "tables.role.member",
+        id: "auth.role.member",
       }),
     },
   };
