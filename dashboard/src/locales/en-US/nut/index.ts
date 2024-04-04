249fd40cfe1ef97529d297c3950f9c801222e19e
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-05 05:23:32 +0630

    add sign-in-up

--- dashboard/src/locales/en-US/nut/index.ts
+++ dashboard/src/locales/en-US/nut/index.ts
@@ -1,8 +1,9 @@
 const items = {
-  "nut.users.sign-in.title": "登录",
-  "nut.users.sign-up.title": "新用户注册",
-  "nut.users.logs.title": "日志列表",
-  "nut.users.forgot-password.title": "忘记密码",
+  "nut.users.sign-in.title": "sign in",
+  "nut.users.sign-up.title": "sign up",
+  "nut.users.sign-in-up.title": "sign in/sign up",
+  "nut.users.logs.title": "logs",
+  "nut.users.forgot-password.title": "forgot password",
 };
 
 export default items;
