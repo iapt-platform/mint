f272bce241d1985566d9126bfd1fdb7f3e5a07ae
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-05 18:49:57 +0630

    add LockFillIcon

--- dashboard/src/assets/icon/index.tsx
+++ dashboard/src/assets/icon/index.tsx
@@ -707,6 +707,28 @@ const TabSvg = () => (
     ></path>
   </svg>
 );
+
+const LockFillSvg = () => (
+  <svg
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    p-id="4253"
+    width="1em"
+    height="1em"
+  >
+    <path
+      d="M748.8 864 275.2 864c-32 0-57.6-25.6-57.6-57.6L217.6 512c0-32 32-57.6 57.6-57.6l467.2 0c38.4 0 57.6 25.6 57.6 57.6l0 294.4C806.4 838.4 780.8 864 748.8 864z"
+      fill="currentColor"
+      p-id="4254"
+    ></path>
+    <path
+      d="M364.8 422.4 364.8 339.2c0-70.4 51.2-121.6 121.6-121.6l51.2 0c64 0 121.6 51.2 121.6 121.6l0 83.2 57.6 0L716.8 339.2c0-102.4-83.2-179.2-179.2-179.2L486.4 160c-102.4 0-179.2 83.2-179.2 179.2l0 83.2L364.8 422.4z"
+      fill="currentColor"
+      p-id="4255"
+    ></path>
+  </svg>
+);
 export const DictIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={DictSvg} {...props} />
 );
@@ -842,4 +864,8 @@ export const AdminIcon = (props: Partial<CustomIconComponentProps>) => (
 
 export const TabIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={TabSvg} {...props} />
+);
+
+export const LockFillIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={LockFillSvg} {...props} />
 );
