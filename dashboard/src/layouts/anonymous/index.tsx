0f4261d6908c5f88b9a008bbd5752ce46987cc14
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-05 18:28:01 +0630

    底部更换为通用foot

--- dashboard/src/layouts/anonymous/index.tsx
+++ dashboard/src/layouts/anonymous/index.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Col, Row } from "antd";
 import UiLangSelect from "../../components/general/UiLangSelect";
 import img_banner from "../../assets/library/images/wikipali_logo_library.svg";
+import FooterBar from "../../components/library/FooterBar";
 
 const Widget = () => {
   return (
@@ -9,19 +10,24 @@ const Widget = () => {
       <div style={{ textAlign: "right", backgroundColor: "#3e3e3e" }}>
         <UiLangSelect />
       </div>
-      <div style={{ paddingTop: "3em", backgroundColor: "#3e3e3e" }}>
+      <div
+        style={{
+          paddingTop: "3em",
+          backgroundColor: "#3e3e3e",
+        }}
+      >
         <Row>
           <Col flex="auto"></Col>
           <Col flex="400px" style={{ padding: "1em" }}>
             <img alt="logo" style={{ height: "5em" }} src={img_banner} />
           </Col>
-          <Col flex="400px" style={{ padding: "1em" }}>
+          <Col flex="400px" style={{ padding: "1em", paddingBottom: 200 }}>
             <Outlet />
           </Col>
           <Col flex="auto"></Col>
         </Row>
       </div>
-      <div>anonymous layout footer</div>
+      <FooterBar />
     </>
   );
 };
