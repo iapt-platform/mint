60b745eb1bd56f5ad51aeb93b4dc82cd4d22c3f3
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-04-07 21:27:27 +0630

    create

---
+++ dashboard/src/pages/studio/home/index.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { Layout } from "antd";
+
+import LeftSider from "../../../components/studio/LeftSider";
+import { styleStudioContent } from "../style";
+import { ProCard } from "@ant-design/pro-components";
+import { useState } from "react";
+import DiscussionListCard from "../../../components/discussion/DiscussionListCard";
+
+const { Content } = Layout;
+
+const Widget = () => {
+  const { studioname } = useParams(); //url 参数
+  const [responsive, setResponsive] = useState(false);
+  return (
+    <Layout>
+      <LeftSider selectedKeys="invite" />
+      <Content style={styleStudioContent}>
+        <ProCard
+          title={studioname}
+          extra="2019年9月28日"
+          split={responsive ? "horizontal" : "vertical"}
+          bordered
+          headerBordered
+        >
+          <ProCard split="horizontal" colSpan="50%">
+            <ProCard title="最近编辑"></ProCard>
+            <ProCard title="新手入门"></ProCard>
+          </ProCard>
+          <ProCard split="horizontal" colSpan="50%">
+            <ProCard title="讨论">
+              <div style={{ minHeight: 360 }}>
+                <DiscussionListCard userId="ddd" />
+              </div>
+            </ProCard>
+          </ProCard>
+        </ProCard>
+      </Content>
+    </Layout>
+  );
+};
+
+export default Widget;
