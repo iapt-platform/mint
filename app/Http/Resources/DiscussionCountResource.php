2e568b2dbc625a80d771f26a237b2db691ff3fba
visuddhinanda <63999293+visuddhinanda@users.noreply.github.com>
2024-05-20 06:49:18 +0800

    添加 wbw信息

--- app/Http/Resources/DiscussionCountResource.php
+++ app/Http/Resources/DiscussionCountResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Wbw;
 
 class DiscussionCountResource extends JsonResource
 {
@@ -14,6 +15,22 @@ class DiscussionCountResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data = [
+            'id' => $this->id,
+            'res_id' => $this->res_id,
+            'res_type' => $this->res_type,
+            'type' => $this->type,
+            'editor_uid' => $this->editor_uid
+        ];
+
+        switch ($this->res_type) {
+            case 'wbw':
+                $wbw = Wbw::where('uid',$this->res_id)
+                        ->select(['book_id','paragraph','wid'])
+                        ->first();
+                $data['wbw'] = $wbw;
+                break;
+        }
+        return $data;
     }
 }
