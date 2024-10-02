804de446fffefacdc6e56149340ff7d48073874b
Jeremy Zheng <jitang.zheng@gmail.com>
2024-10-02 04:39:43 +0000

    :wrench: add env arg for docker

--- docker/open-search/build.sh
+++ docker/open-search/build.sh
@@ -5,8 +5,13 @@ set -e
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 export CODE="mint-open-search"
 
+if [ "$#" -ne 1 ]; then
+    echo "USAGE: $0 OPENSEARCH_ARCH"
+    exit 1
+fi
+
 podman pull ubuntu:latest
-podman build -t $CODE .
+podman build --build-arg OPENSEARCH_ARCH=$1 -t $CODE .
 podman save --format=oci-archive -o $CODE-$VERSION.tar $CODE
 md5sum $CODE-$VERSION.tar >$CODE-$VERSION.md5
 
