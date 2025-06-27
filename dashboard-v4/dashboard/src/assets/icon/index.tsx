b9388aee0d64d1a6c830855673aa6c64351d5dd4
visuddhinanda <visuddhinanda@gmail.com>
2025-06-27 17:08:40 +0000

    add more icon

--- dashboard-v4/dashboard/src/assets/icon/index.tsx
+++ dashboard-v4/dashboard/src/assets/icon/index.tsx
@@ -871,6 +871,33 @@ const GroupSvg = () => (
     ></path>
   </svg>
 );
+
+const MoreSvg = () => (
+  <svg
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    p-id="5057"
+    width="1em"
+    height="1em"
+  >
+    <path
+      d="M227.14123 413.647995c-52.14973 0-94.587262 42.439578-94.587262 94.587262 0 52.14973 42.437531 94.587262 94.587262 94.587262 52.147684 0 94.587262-42.437531 94.587262-94.587262C321.728492 456.087573 279.288914 413.647995 227.14123 413.647995z"
+      fill="currentColor"
+      p-id="5058"
+    ></path>
+    <path
+      d="M510.903016 413.647995c-52.14973 0-94.587262 42.439578-94.587262 94.587262 0 52.14973 42.437531 94.587262 94.587262 94.587262 52.147684 0 94.587262-42.437531 94.587262-94.587262C605.490278 456.087573 563.051723 413.647995 510.903016 413.647995z"
+      fill="currentColor"
+      p-id="5059"
+    ></path>
+    <path
+      d="M794.665825 413.647995c-52.14973 0-94.587262 42.439578-94.587262 94.587262 0 52.14973 42.437531 94.587262 94.587262 94.587262 52.147684 0 94.587262-42.437531 94.587262-94.587262C889.253086 456.087573 846.813508 413.647995 794.665825 413.647995z"
+      fill="currentColor"
+      p-id="5060"
+    ></path>
+  </svg>
+);
 export const DictIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={DictSvg} {...props} />
 );
@@ -1038,4 +1065,8 @@ export const SortIcon = (props: Partial<CustomIconComponentProps>) => (
 
 export const GroupIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={GroupSvg} {...props} />
+);
+
+export const MoreIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={MoreSvg} {...props} />
 );
