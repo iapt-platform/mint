2d4fb33d43d8861a920bf55bc7cb137cd8197e9e
Jeremy Zheng <jitang.zheng@gmail.com>
2025-06-29 09:24:14 +0000

    :wrench: create api v12 image

--- ai-translate/docker/build.sh
+++ ai-translate/docker/build.sh
@@ -9,11 +9,11 @@ fi
 
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 export CODE="mint-python$1"
-export TAR="$CODE-$VERSION"
+export TAR="$CODE-$VERSION-$(uname -m)"
 
 podman pull ubuntu:latest
 podman build --build-arg PYTHON_VERSION=$1 -t $CODE .
-podman save --format=oci-archive -o $TAR-$(uname -m).tar $CODE
+podman save --format=oci-archive -o $TAR.tar $CODE
 md5sum $TAR.tar >$TAR.md5
 
 echo "done($TAR.tar)."
