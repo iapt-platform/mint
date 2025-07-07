82faf26efd39f961c495d8ce737d46dad29edb2f
Jeremy Zheng <jitang.zheng@gmail.com>
2025-07-07 22:05:22 +0000

    :wrench: split configuration reader

--- open-ai-server/src/index.js
+++ open-ai-server/src/index.js
@@ -1,18 +1,6 @@
-import progress from "node:process";
-import fs from "node:fs";
-
 import logger from "./logger";
 import server from "./server";
-
-if (progress.argv.length !== 3) {
-  logger.error("USAGE: node main-XXX.js config.json");
-  process.exit(1);
-}
-
-const config_file = progress.argv[2];
-const config = JSON.parse(fs.readFileSync(config_file, "utf8"));
-logger.info(`load config from ${config_file}`);
-logger.debug("run on debug mode");
+import config from "./config";
 
 const port = config["port"];
 server.listen(port, () => {
