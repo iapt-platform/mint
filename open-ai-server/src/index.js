f798f4f4607b78cfc3f6ddc808dde23913ba6f2b
visuddhinanda <visuddhinanda@gmail.com>
2025-07-17 11:21:36 +0000

    add log API Server Url

--- open-ai-server/src/index.js
+++ open-ai-server/src/index.js
@@ -7,4 +7,5 @@ server.listen(port, () => {
   logger.info("Server is running on port %d", port);
   logger.info("Health check: http://0.0.0.0:%d/health", port);
   logger.info("API endpoint: http://0.0.0.0:%d/api/openai", port);
+  logger.info("API Server Url: %s", config["api_url"]);
 });
