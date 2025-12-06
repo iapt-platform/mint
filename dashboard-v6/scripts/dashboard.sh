4d50f28c50bb3c33e2f121099433d89311541591
Jeremy Zheng <jitang.zheng@gmail.com>
2025-12-06 23:13:48 +0000

    :beers: add antd & antd-pro support(dependency conflicts)

---
+++ dashboard-v6/scripts/dashboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+npm install --save \
+    react-router react-intl @reduxjs/toolkit react-redux \
+    @graphiql/react \
+    usehooks-ts jose \
+    js-cookie @types/js-cookie \
+    antd @ant-design/pro-components
+
+exit 0
--- dashboard-v6/vite.config.ts
+++ dashboard-v6/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pcd-v2/",
+  server: {
+    host: "127.0.0.1",
+    port: 4000,
+    proxy: {
+      "/api": "http://127.0.0.1:8080",
+    },
+  },
   plugins: [react()],
-})
+});
