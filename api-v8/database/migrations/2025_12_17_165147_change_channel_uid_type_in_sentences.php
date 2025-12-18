13e52f97594bd6bb41d9f0614190c3ad46460862
visuddhinanda <visuddhinanda@gmail.com>
2025-12-18 08:51:56 +0000

    create

---
+++ api-v8/database/migrations/2025_12_17_165147_change_channel_uid_type_in_sentences.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class ChangeChannelUidTypeInSentences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sentences', function (Blueprint $table) {
+            DB::statement("
+            ALTER TABLE sentences
+            ALTER COLUMN channel_uid TYPE uuid
+            USING CASE
+                WHEN channel_uid = '' THEN NULL
+                ELSE channel_uid::uuid
+            END
+        ");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sentences', function (Blueprint $table) {
+            DB::statement('ALTER TABLE sentences ALTER COLUMN channel_uid TYPE varchar(255) USING channel_uid::varchar');
+        });
+    }
+}
