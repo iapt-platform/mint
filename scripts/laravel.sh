5ab071880dbb9e2f1881baf24427f2ed95ec58be
China Kassapa <ven.kassapa@qq.com>
2026-01-08 05:53:57 +0000

    :heavy_plus_sign: add third dependencies

---
+++ scripts/laravel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# git config --global --add safe.directory /workspace
+composer require guzzlehttp/guzzle
+composer require opensearch-project/opensearch-php
+composer require phpoffice/phpspreadsheet
+composer require aws/aws-sdk-php
+composer require firebase/php-jwt
+composer require casbin/casbin
+
+exit 0
--- scripts/third-dependencies-pack.sh
+++ scripts/third-dependencies-pack.sh
@@ -4,15 +4,14 @@ set -e
 
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 
-if [[ "$#" -eq 1 && "$1" == "dashboard" ]]; then
-    XZ_OPT=-9 tar -cJf dashboard-v6-$VERSION.tar.xz -C dashboard-v6 node_modules package-lock.json
-    XZ_OPT=-9 tar -cJf dashboard-v4-$VERSION.tar.xz -C dashboard-v4/dashboard node_modules yarn.lock
-elif [[ "$#" -eq 1 && "$1" == "laravel" ]]; then
-    XZ_OPT=-9 tar -cJf dashboard-$VERSION.tar.xz -C xxx node_modules package-lock.json
-else
-    echo "USAGE: $0 dashboard|laravel"
-    exit 1
-fi
+XZ_OPT=-9 tar -cJf mint-$VERSION
+
+export TAR="XZ_OPT=-9 tar -cJf"
+
+$TAR dashboard-v6-$VERSION.tar.xz -C dashboard-v6 node_modules package-lock.json
+$TAR dashboard-v4-$VERSION.tar.xz -C dashboard-v4/dashboard node_modules yarn.lock
+$TAR api-v8-frontend-$VERSION.tar.xz -C api-v8 node_modules package-lock.json
+$TAR api-v8-public-$VERSION.tar.xz -C dashboard-v4/dashboard node_modules yarn.lock
 
 exit 0
 
