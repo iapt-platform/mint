599a3ce58a3c8bcbf4f2525e3c0f285c5cc49356
visuddhinanda <visuddhinanda@gmail.com>
2026-01-11 15:05:49 +0000

    add Middleware

--- api-v12/bootstrap/app.php
+++ api-v12/bootstrap/app.php
@@ -5,14 +5,20 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        // API 组中间件（来自原 Laravel 8 Kernel）
+        $middleware->api(append: [
+            \App\Http\Middleware\ApiLog::class,
+            \App\Http\Middleware\UserOperation::class,
+        ]);
+
+        // web 组中间件
+        $middleware->web(append: [
+            \App\Http\Middleware\SetLocale::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
