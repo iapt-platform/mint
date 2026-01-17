e3a3986dbf08f29cf32547ae874878b7b12a026f
China Kassapa <ven.kassapa@qq.com>
2026-01-17 05:29:48 +0000

    :bug: fix php-fpm container

--- deploy/roles/mint-v2.3/files/php-fpm.sh
+++ deploy/roles/mint-v2.3/files/php-fpm.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-# TODO
-sleep 60*60*24
+sed -i 's/^listen = .*/listen = 9000/g' /etc/php/$1/fpm/pool.d/www.conf
+
+/usr/sbin/php-fpm${1} --nodaemonize --fpm-config /etc/php/$1/fpm/php-fpm.conf
 
 exit 0
