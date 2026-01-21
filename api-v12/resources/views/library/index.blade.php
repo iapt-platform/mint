d268a3f84506526915c36a185610ee19c2c10a9b
visuddhinanda <visuddhinanda@gmail.com>
2026-01-21 15:49:49 +0000

    一级分类横排，添加最新更新

--- api-v12/resources/views/library/index.blade.php
+++ api-v12/resources/views/library/index.blade.php
@@ -14,29 +14,54 @@
             </div>
         </div>
 
-        @foreach($categoryData as $data)
-        <div class="card mb-4">
-            <div class="card-header">
-                <h3 class="card-title">
-                    <svg class="icon me-2" width="24" height="24">
-                        <use xlink:href="#tabler-book"></use>
-                    </svg>
-                    {{ $data['category']['name'] }}
-                </h3>
-                <div class="card-actions">
-                    <a href="{{ route('library.category.show', $data['category']['id']) }}" class="btn btn-primary btn-sm">
-                        {{ __('buttons.more') }}
-                        <svg class="icon ms-1" width="24" height="24">
-                            <use xlink:href="#tabler-arrow-right"></use>
-                        </svg>
-                    </a>
+        {{-- ✅ 所有卡片的统一容器 --}}
+        <div class="row g-4">
+            @foreach($categoryData as $data)
+            {{-- ✅ 响应式列 --}}
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <svg class="icon me-2" width="24" height="24">
+                                <use xlink:href="#tabler-book"></use>
+                            </svg>
+                            {{ $data['category']['name'] }}
+                        </h3>
+                        <div class="card-actions">
+                            <a href="{{ route('library.category.show', $data['category']['id']) }}"
+                                class="btn btn-primary btn-sm">
+                                {{ __('buttons.more') }}
+                                <svg class="icon ms-1" width="24" height="24">
+                                    <use xlink:href="#tabler-arrow-right"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        @foreach($data['children'] as $child)
+                        <div class="mb-1">
+                            <a href="{{ route('library.category.show', $child['id']) }}">
+                                {{ $child['name'] }}
+                            </a>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
+            @endforeach
+        </div>
+
+        <div class="card h-100">
+            <div class="card-header">
+                <h3 class="card-title">
+                    update
+                </h3>
+            </div>
             <div class="card-body">
-                @include('components.book-list', ['books' => $data['books']])
+                @include('components.book-list', ['books' => $update])
             </div>
         </div>
-        @endforeach
     </div>
 </div>
 @endsection
