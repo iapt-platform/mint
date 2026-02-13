049748505c015139df3197a4536be76f494381d2
China Kassapa <ven.kassapa@qq.com>
2026-02-13 23:21:37 +0000

    :heavy_plus_sign: add antd pro v3

--- dashboard-v6/scripts/dashboard.sh
+++ dashboard-v6/scripts/dashboard.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-# @ant-design/pro-components
 npm install --save \
     react-router react-intl @reduxjs/toolkit react-redux \
     @graphiql/react \
     usehooks-ts jose \
     js-cookie @types/js-cookie \
-    antd @ant-design/x
+    antd @ant-design/x \
+    rc-virtual-list
+# FIXME
+npm install --save @ant-design/pro-components@beta
+
+npm ls
 
 exit 0
