389e9818f2e15cbfac26f9eddab1bd32f009503c
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-17 03:22:32 +0000

    :heavy_plus_sign: add lodash library

--- dashboard-v6/scripts/dashboard.sh
+++ dashboard-v6/scripts/dashboard.sh
@@ -6,7 +6,7 @@ npm install --save \
     react-router react-intl @reduxjs/toolkit react-redux \
     @graphiql/react usehooks-ts rc-virtual-list \
     jose dayjs \
-    js-cookie @types/js-cookie diff @types/diff marked @types/marked video.js @types/video.js \
+    lodash @types/lodash js-cookie @types/js-cookie diff @types/diff marked @types/marked video.js @types/video.js \
     antd @ant-design/x @ant-design/charts @ant-design/charts @ant-design/pro-components@beta
 
 npm ls
