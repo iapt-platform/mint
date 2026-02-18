76477c7b5ec9265f86049a71f788c583245e44cf
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-18 09:34:27 +0000

    :heavy_plus_sign: add markdpwm & html editor support

--- dashboard-v6/scripts/dashboard.sh
+++ dashboard-v6/scripts/dashboard.sh
@@ -6,6 +6,8 @@ npm install --save \
     react-router react-intl @reduxjs/toolkit react-redux \
     @graphiql/react usehooks-ts rc-virtual-list \
     jose dayjs \
+    remark-gfm react-markdown @uiw/react-md-editor @mdxeditor/editor \
+    slate slate-history slate-react \
     lodash @types/lodash js-cookie @types/js-cookie diff @types/diff marked @types/marked video.js @types/video.js \
     antd @ant-design/x @ant-design/charts @ant-design/charts @ant-design/pro-components@beta
 
