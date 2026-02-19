482ae9d425ea501e79ede79ad300e08027b36926
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-19 01:59:41 +0000

    :heavy_plus_sign: add antd-plots

--- dashboard-v6/scripts/dashboard.sh
+++ dashboard-v6/scripts/dashboard.sh
@@ -9,7 +9,7 @@ npm install --save \
     remark-gfm react-markdown @uiw/react-md-editor @mdxeditor/editor \
     slate slate-history slate-react \
     lodash @types/lodash js-cookie @types/js-cookie diff @types/diff marked @types/marked video.js @types/video.js \
-    antd @ant-design/x @ant-design/charts @ant-design/charts @ant-design/pro-components@beta
+    antd @ant-design/x @ant-design/charts @ant-design/plots @ant-design/pro-components@beta
 
 npm ls
 
