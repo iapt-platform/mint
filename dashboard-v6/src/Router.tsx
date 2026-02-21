c61e5145e5565d9643e672a212fbd5303b41d593
visuddhinanda <visuddhinanda@gmail.com>
2026-02-21 01:48:13 +0000

    add Workspace

--- dashboard-v6/src/Router.tsx
+++ dashboard-v6/src/Router.tsx
@@ -10,6 +10,7 @@ const Home = lazy(() => import("./pages/home"));
 const RootLayout = lazy(() => import("./layouts/Root"));
 const AnonymousLayout = lazy(() => import("./layouts/anonymous"));
 const DashboardLayout = lazy(() => import("./layouts/dashboard"));
+const WorkspaceLayout = lazy(() => import("./layouts/workspace"));
 
 const router = createBrowserRouter(
   [
@@ -31,6 +32,25 @@ const router = createBrowserRouter(
             {
               path: "users",
               children: [{ path: "personal", Component: UsersPersonal }],
+            },
+          ],
+        },
+        {
+          path: "workspace",
+          Component: WorkspaceLayout,
+          children: [
+            { index: true, Component: WorkspaceLayout },
+            {
+              path: "home",
+              Component: UsersPersonal,
+            },
+            {
+              path: "ai",
+              Component: UsersPersonal,
+            },
+            {
+              path: "tipitaka",
+              Component: UsersPersonal,
             },
           ],
         },
