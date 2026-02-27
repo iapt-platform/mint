48916afe306c8a355803a26225d56bb976350e22
Jeremy Zheng <jitang.zheng@gmail.com>
2026-02-27 00:37:38 +0000

    :heavy_plus_sign: add antd x sdk & markdown

--- dashboard-v6/scripts/dashboard.sh
+++ dashboard-v6/scripts/dashboard.sh
@@ -9,7 +9,7 @@ npm install --save \
     remark-gfm react-markdown @uiw/react-md-editor @mdxeditor/editor \
     slate slate-history slate-react \
     lodash @types/lodash js-cookie @types/js-cookie diff @types/diff marked @types/marked \
-    antd @ant-design/x @ant-design/charts @ant-design/plots @ant-design/pro-components@beta
+    antd @ant-design/x @ant-design/x-sdk @ant-design/x-markdown @ant-design/charts @ant-design/plots @ant-design/pro-components@beta
 
 npm ls
 
