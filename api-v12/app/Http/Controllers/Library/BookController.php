81a6d89806cb4f5e4a9abf5d09a3605f845f77bd
visuddhinanda <visuddhinanda@gmail.com>
2026-04-15 09:00:20 +0000

    :fire: use Illuminate\Support\Facades\Storage;

--- api-v12/app/Http/Controllers/Library/BookController.php
+++ api-v12/app/Http/Controllers/Library/BookController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Library;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use App\Models\ProgressChapter;
 use App\Models\PaliText;
 use App\Models\Sentence;
