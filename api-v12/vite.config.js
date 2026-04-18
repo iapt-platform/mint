26da1757ef7dfb74f2044d693a5436066d5f18d4
visuddhinanda <visuddhinanda@gmail.com>
2026-04-18 12:01:35 +0000

    app.css -> main.css

--- api-v12/vite.config.js
+++ api-v12/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/main.css", "resources/js/app.js"],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
