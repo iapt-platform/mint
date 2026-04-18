85d1984b63ab498d9cf76d338c3f74e470987ff5
visuddhinanda <visuddhinanda@gmail.com>
2026-04-18 02:59:51 +0000

    add query_info

--- api-v12/app/DTO/Search/SearchResultDTO.php
+++ api-v12/app/DTO/Search/SearchResultDTO.php
@@ -7,13 +7,15 @@ class SearchResultDTO
     public function __construct(
         public bool $success,
         public SearchDataDTO $data,
+        public QueryInfoDTO $query_info,
     ) {}
 
     public static function fromArray(array $data): self
     {
         return new self(
             success: $data['success'],
-            data: SearchDataDTO::fromArray($data['data'])
+            data: SearchDataDTO::fromArray($data['data']),
+            query_info: QueryInfoDTO::fromArray($data['query_info']),
         );
     }
 }
