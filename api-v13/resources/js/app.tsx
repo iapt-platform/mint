31cdd5613733be2690ae66a3cef8b4cbcd0a876d
Jeremy Zheng <jitang.zheng@gmail.com>
2026-04-19 08:46:26 +0000

    :tada: add laravel-v13 project

---
+++ api-v13/resources/js/app.tsx
@@ -0,0 +1,10 @@
+import { createInertiaApp } from '@inertiajs/react';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    progress: {
+        color: '#4B5563',
+    },
+});
