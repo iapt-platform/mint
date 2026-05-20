8be3897f407c0507b0dca03e0ac0f98bd5335e9e
visuddhinanda <visuddhinanda@gmail.com>
2026-05-20 10:39:49 +0000

    :fire: 废弃的use

--- api-v13/app/Http/Controllers/ApiController.php
+++ api-v13/app/Http/Controllers/ApiController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Facades\Log;
+
 
 class ApiController extends Controller
 {
