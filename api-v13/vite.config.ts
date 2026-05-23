3eb312c21cde8163566da31dccf5a61e3fe0a7c4
Jeremy Zheng <jitang.zheng@gmail.com>
2026-05-23 13:32:43 +0000

    :bug: fix library-index.css 404

--- api-v13/vite.config.ts
+++ api-v13/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/library.css', // library/* + blog 列表页
+		'resources/css/modules/library-index.css',
                 'resources/css/reader.css', // 全站阅读页（待建）
                 'resources/js/app.js',
             ],
