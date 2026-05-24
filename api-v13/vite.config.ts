93e8745291254f6a9514d1e991753f913ba0b40e
Jeremy Zheng <jitang.zheng@gmail.com>
2026-05-24 01:38:19 +0000

    :bug: fix api-v13 frontend building

--- api-v13/vite.config.ts
+++ api-v13/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
                 'resources/css/modules/tipitaka.css',
                 'resources/css/modules/anthology.css',
                 'resources/css/reader.css', // 全站阅读页（待建）
-                'resources/css/main.css', // 全站阅读页（待建）
+                // 'resources/css/main.css', // 全站阅读页（待建）
                 'resources/js/app.js',
                 'resources/js/reader.js',
                 'resources/js/modules/term-tooltip.js',
